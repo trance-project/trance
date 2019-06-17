c51ecd97b104207ebdd8626efa24406f1a052a29
Jaclyn Smith <jacalacka@gmail.com>
2019-06-17 14:00:17 +0100

    simple evaluator

--- src/main/scala/shredding/wmcc/App.scala
+++ src/main/scala/shredding/wmcc/App.scala
@@ -1,6 +1,6 @@
 package shredding.wmcc
 
-import shredding.examples.simple.FlatTests
+import shredding.examples.simple.{FlatTests, FlatRelations}
 import shredding.examples.simple.NestedTests
 
 object App {
@@ -12,8 +12,10 @@ object App {
     val q1 = translator.translate(FlatTests.q1.asInstanceOf[translator.Expr])
     val normq1 = normalizer.finalize(q1)
     println(Printer.quote(normq1.asInstanceOf[CExpr]))
-    // val evaluator = new Finalizer(new BaseScalaInter {})
-    // println(evaluator.finalize(normq1.asInstanceOf[CExpr]))
+    val eval = new BaseScalaInterp{}
+    val evaluator = new Finalizer(eval)
+    eval.ctx("R") = FlatRelations.format1a
+    println(evaluator.finalize(normq1.asInstanceOf[CExpr]))
   }
 
 
