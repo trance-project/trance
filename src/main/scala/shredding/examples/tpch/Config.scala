010b46c3fce0b345fc082c011adfb2cb77b9f6cf
Jaclyn Smith <jacmarjorie@gmail.com>
2019-08-01 08:10:46 -0700

    removing unnecessary outfile

--- src/main/scala/shredding/examples/tpch/Config.scala
+++ src/main/scala/shredding/examples/tpch/Config.scala
@@ -2,7 +2,7 @@ package shredding.examples.tpch
 
 object Config {
   val prop = new java.util.Properties
-  val fsin = new java.io.FileInputStream("data.flat")
+  val fsin = new java.io.FileInputStream("/mnt/app_hdd/scratch/tpch/shredder/data.flat")
   prop.load(fsin)
   val datapath = prop.getProperty("datapath")
 }
