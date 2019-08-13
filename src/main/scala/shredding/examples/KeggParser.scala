21ebf7e90438e9dcd710d9e81ba4ee311d3e5002
Gabor <gabor.gyorkei@gmail.com>
2019-08-13 16:59:40 +0100

    Undoing unwanted commit

--- src/main/scala/shredding/examples/KeggParser.scala
+++ src/main/scala/shredding/examples/KeggParser.scala
@@ -5,7 +5,8 @@ import org.apache.spark.{SparkConf, SparkContext}
 object KeggParser {
 
   /** defines a genomic path with name and description url.
-    *
+    * the file /home/gabor/Downloads/c2.cp.v6.2.symbols.gmt is downloaded from
+    *     http://software.broadinstitute.org/gsea/msigdb/download_file.jsp?filePath=/resources/msigdb/6.2/c2.cp.v6.2.symbols.gmt
     * @param sName          the name. For example: KEGG_GLYCOLYSIS_GLUCONEOGENESIS
     * @param descriptionUrl example: http://www.broadinstitute.org/gsea/msigdb/cards/KEGG_GLYCOLYSIS_GLUCONEOGENESIS
     * @param path           example: ACSS2	GCK	PGK2	PGK1	PDHB	PDHA1	PDHA2	PGM2	TPI1	ACSS1	FBP1	ADH1B	HK2	ADH1C	HK1	HK3	ADH4	PGAM2	ADH5	PGAM1	ADH1A [...]
