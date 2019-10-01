91a3e483af915b999a51a53207c2dde53b304877
Gabor <gabor.gyorkei@gmail.com>
2019-10-01 09:39:49 +0100

    GDBLoader

---
+++ src/main/scala/shredding/gdbloader/App.scala
@@ -0,0 +1,23 @@
+package gdbloader
+
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
+
+object App{
+  def main(args: Array[String]){
+    println("gdbLoaderTest")
+//    val conf = new SparkConf().setMaster("local[*]").setAppName("gdbLoaderTest")
+    val conf = new SparkConf().setMaster("spark://192.168.11.247:7077").setAppName("gdbLoaderTest")
+
+    val spark = SparkSession.builder().config(conf).getOrCreate()
+    val gdbLoader = GDBLoader(spark)
+    val regions = List("rs12415800", "rs35936514")
+    val l =GDBLoader (spark)
+    val res = l.executeQuery(regions)
+    println("gdbLoaderTest results:")
+    println("Kegg data: " )
+    l.parseKeg("c2.cp.v6.2.symbols.gmt").collect().foreach(println(_))
+    res.foreach(println(_))
+
+  }
+}
