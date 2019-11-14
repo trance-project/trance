a90db6f078a753a95d38b80af592bc787877878c
Jaclyn Smith <jacalacka@gmail.com>
2019-11-14 15:02:24 +0000

    starting nested to nested

--- src/main/scala/shredding/generator/App.scala
+++ src/main/scala/shredding/generator/App.scala
@@ -26,34 +26,29 @@ object App {
  
   def runTPCH1(){
 
-    println(" --------------------------- TPCH Query 1 Original ------------------------")
+    /**
     Utils.runSparkNew(TPCHQuery1Full)
     Utils.runSparkNew(TPCHQuery1Full, shred = true)
-
-
-    println(" --------------------------- TPCH Query 1 Optimized-------------------------")  
     Utils.runSparkNew(TPCHQuery1)
-    Utils.runSparkNew(TPCHQuery1, shred = true)
+    Utils.runSparkNew(TPCHQuery1, shred = true)**/
 
     /**Utils.runSparkInputNew(TPCHQuery4Inputs, TPCHQuery4)
     Utils.runSparkInputNew(TPCHQuery4Inputs, TPCHQuery4, shred = true)**/
   
+    /**Utils.runSparkNew(TPCHQuery2)
+    Utils.runSparkNew(TPCHQuery2, shred = true)
+    Utils.runSparkNew(TPCHQuery2Full)
+    Utils.runSparkNew(TPCHQuery2Full, shred = true)**/
+ 
     /**Utils.runSparkNew(TPCHQuery3Full)
     Utils.runSparkNew(TPCHQuery3Full, shred = true)
-
     Utils.runSparkNew(TPCHQuery3)
-    Utils.runSparkNew(TPCHQuery3, shred = true)
-
-    Utils.runSparkNew(TPCHQuery2)
-    Utils.runSparkNew(TPCHQuery2, shred = true)
-
-    Utils.runSparkNew(TPCHQuery2Full)
-    Utils.runSparkNew(TPCHQuery2Full, shred = true)
-    
+    Utils.runSparkNew(TPCHQuery3, shred = true)**/
+ 
     Utils.runSparkInputNew(TPCHQuery2, TPCHQuery6Full)
     Utils.runSparkInputNew(TPCHQuery2, TPCHQuery6Full, shred = true)
 
-    Utils.runSparkInputNew(TPCHQuery3, TPCHQuery7Full)**/
+    //Utils.runSparkInputNew(TPCHQuery3, TPCHQuery7Full)
     //Utils.runSparkInputNew(TPCHQuery3, TPCHQuery7Full, shred = true)
   }
 
