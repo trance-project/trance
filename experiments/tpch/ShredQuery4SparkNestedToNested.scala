3b545bf8966080080286e1fa3644ec8c6f682bad
Jaclyn Smith <jacalacka@gmail.com>
2019-11-25 13:49:44 +0000

    updated nested to nested query 4

--- experiments/tpch/ShredQuery4SparkNestedToNested.scala
+++ experiments/tpch/ShredQuery4SparkNestedToNested.scala
@@ -8,11 +8,12 @@ import sprkloader.SkewPairRDD._
 case class Record23(c__Fc_custkey: Int)
 case class Record24(c_name: String, corders: Record23)
 case class Record25(o_orderkey: Int, o_orderdate: String)
-case class Record87(customer__Fcorders: Record23)
-case class Record88(c_name: String, partqty: Record87)
-case class Record89(l_quantity: Double, l_orderkey: Int, l_partkey: Int)
-case class Record90(p_name: String, p_partkey: Int)
-case class Record92(orderdate: String, pname: String)
+case class Record100(l_orderkey: Int, l_quantity: Double, l_partkey: Int)
+case class Record101(p_name: String, p_partkey: Int)
+case class Record102(l_orderkey: Int, p_name: String, l_qty: Double)
+case class Record103(customer__Fcorders: Record23)
+case class Record104(c_name: String, partqty: Record103)
+case class Record106(orderdate: String, pname: String)
 object ShredQuery4SparkNestedToNested {
  def main(args: Array[String]){
    val sf = Config.datapath.split("/").last
@@ -66,56 +67,65 @@ val CustOrders__D_2corders_1 = M_flat2
 CustOrders__D_2corders_1.cache
 CustOrders__D_2corders_1.count
 def f = { 
- val x49 = CustOrders__D_1.map{ case x44 => 
-   val x45 = x44.c_name 
-val x46 = x44.corders 
-val x47 = Record87(x46) 
-val x48 = Record88(x45, x47) 
-x48 
+ val x52 = L__D_1.map(x47 => { val x48 = x47.l_orderkey 
+val x49 = x47.l_quantity 
+val x50 = x47.l_partkey 
+val x51 = Record100(x48, x49, x50) 
+x51 }) 
+val x57 = P__D_1.map(x53 => { val x54 = x53.p_name 
+val x55 = x53.p_partkey 
+val x56 = Record101(x54, x55) 
+x56 }) 
+val x62 = { val out1 = x52.map{ case x58 => ({val x60 = x58.l_partkey 
+x60}, x58) }
+  val out2 = x57.map{ case x59 => ({val x61 = x59.p_partkey 
+x61}, x59) }
+  out1.joinSkewLeft(out2).map{ case (k,v) => v }
 } 
-val M_flat1 = x49
-val x50 = M_flat1
-M_flat1.count
+val x69 = x62.map{ case (x63, x64) => 
+   val x65 = x63.l_orderkey 
+val x66 = x64.p_name 
+val x67 = x63.l_quantity 
+val x68 = Record102(x65, x66, x67) 
+x68 
+} 
+val parts__D_1 = x69
+val x70 = parts__D_1
+//parts__D_1.collect.foreach(println(_))
+val x76 = CustOrders__D_1.map{ case x71 => 
+   val x72 = x71.c_name 
+val x73 = x71.corders 
+val x74 = Record103(x73) 
+val x75 = Record104(x72, x74) 
+x75 
+} 
+val M_flat1 = x76
+val x77 = M_flat1
 //M_flat1.collect.foreach(println(_))
-val x56 = L__D_1.map(x51 => { val x52 = x51.l_quantity 
-val x53 = x51.l_orderkey 
-val x54 = x51.l_partkey 
-val x55 = Record89(x52, x53, x54) 
-x55 }) 
-val x61 = P__D_1.map(x57 => { val x58 = x57.p_name 
-val x59 = x57.p_partkey 
-val x60 = Record90(x58, x59) 
-x60 }) 
-val x66 = { val out1 = x56.map{ case x62 => ({val x64 = x62.l_partkey 
-x64}, x62) }
-  val out2 = x61.map{ case x63 => ({val x65 = x63.p_partkey 
-x65}, x63) }
+val x78 = CustOrders__D_2corders_1 
+val x79 = x78 
+val x81 = x79 
+val x83 = parts__D_1 
+val x88 = { val out1 = x81.flatMap{ case (x82, v) => v.map{ case x84 => ({val x86 = x84.o_orderkey 
+x86}, x84) }}
+  val out2 = x83.map{ case x85 => ({val x87 = x85.l_orderkey 
+x87}, x85) }
   out1.join(out2).map{ case (k,v) => v }
 } 
-val x67 = CustOrders__D_2corders_1
-val x68 = x67
-val x74 = { val out1 = x66.map{ case (x69, x70) => ({val x73 = x69.l_orderkey 
-x73}, (x69, x70)) }
-  val out2 = x68
-        /** WHEN DOES THIS CASE HAPPEN **/
-        .flatMap(v2 => v2._2.map{case x71 => ({val x72 = x71.o_orderkey 
-x72}, x71)})
-  out1.lookup(out2)
-} 
-val x84 = x74.flatMap{ case ((x75, x76), x77) => val x83 = (x77,x76) 
-x83 match {
+val x97 = x88.flatMap{ case (x89, x90) => val x96 = (x89,x90) 
+x96 match {
    case (_,null) => Nil
-   case x82 => List(({val x78 = x77.o_orderdate 
-val x79 = x76.p_name 
-val x80 = Record92(x78, x79) 
-x80}, {val x81 = x75.l_quantity 
-x81}))
+   case x95 => List(({val x91 = x89.o_orderdate 
+val x92 = x90.p_name 
+val x93 = Record106(x91, x92) 
+x93}, {val x94 = x90.l_qty 
+x94}))
  }
 }.reduceByKey(_ + _) 
-val M_flat2 = x84
-val x85 = M_flat2
+val M_flat2 = x97
+val x98 = M_flat2
 //M_flat2.collect.foreach(println(_))
-x85.count
+x98.count
 }
 var start0 = System.currentTimeMillis()
 f
