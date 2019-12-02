b5985a00c3d767442452c540d9177e1b856d9119
Jaclyn Smith <jacalacka@gmail.com>
2019-12-02 14:01:41 +0000

    query 2 with new result inner and proper type for mflat2

--- experiments/tpch/ShredQuery2SparkOpt.scala
+++ experiments/tpch/ShredQuery2SparkOpt.scala
@@ -1,29 +1,17 @@
 
 package experiments
-/** 
-resultInner = For o in O Union
-  For c in C Union
-    If (c.c_custkey = o.o_custkey)
-    Then Sng((o_orderkey := o.o_orderkey, c_name := c.c_name))
-
-For s in S Union
-  Sng((s_name := s.s_name, customers2 := For l in L Union
-    If (s.s_suppkey = l.l_suppkey)
-    Then For co in resultInner Union
-      If (co.o_orderkey = l.l_orderkey)
-      Then Sng((c_name2 := co.c_name))))
-**/
+/** Generated **/
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
 import sprkloader._
 import sprkloader.SkewPairRDD._
-case class Record143(o_orderkey: Int, o_custkey: Int)
-case class Record144(c_name: String, c_custkey: Int)
-case class Record145(o_orderkey: Int, c_name: String)
-case class Record146(s__Fs_suppkey: Int)
-case class Record147(s_name: String, customers2: Record146)
-case class Record148(l_suppkey: Int, l_orderkey: Int)
-case class Record150(c_name2: String)
+case class Record164(l_suppkey: Int, l_orderkey: Int)
+case class Record165(o_custkey: Int, o_orderkey: Int)
+case class Record166(c_name: String, c_custkey: Int)
+case class Record167(l_suppkey: Int, c_name: String)
+case class Record168(s__Fs_suppkey: Int)
+case class Record169(s_name: String, customers2: Record168)
+case class Record170(c_name2: String)
 object ShredQuery2SparkOpt {
  def main(args: Array[String]){
    val sf = Config.datapath.split("/").last
@@ -48,89 +36,62 @@ S__D_1.cache
 S__D_1.count
 
    def f = { 
-/**
-resultInner__D_1 :=  REDUCE[ (o_orderkey := x75.o_orderkey,c_name := x76.c_name) / true ]( <-- (x75,x76) -- (
- <-- (x75) -- SELECT[ true, (o_orderkey := x75.o_orderkey,o_custkey := x75.o_custkey) ](O__D._1)) JOIN[x75.o_custkey = x76.c_custkey](
-
-   <-- (x76) -- SELECT[ true, (c_name := x76.c_name,c_custkey := x76.c_custkey) ](C__D._1)))
-**/
- val x81 = O__D_1 
-val x86 = x81.map(x82 => { val x83 = x82.o_orderkey 
-val x84 = x82.o_custkey 
-val x85 = Record143(x83, x84) 
-x85 }) 
-val x87 = C__D_1 
-val x92 = x87.map(x88 => { val x89 = x88.c_name 
-val x90 = x88.c_custkey 
-val x91 = Record144(x89, x90) 
-x91 }) 
-val x97 = { val out1 = x86.map{ case x93 => ({val x95 = x93.o_custkey 
-x95}, x93) }
-  val out2 = x92.map{ case x94 => ({val x96 = x94.c_custkey 
-x96}, x94) }
-  out1.joiniSkewLeft(out2).map{ case (k,v) => v }
-} 
-val x103 = x97.map{ case (x98, x99) => 
-   val x100 = x98.o_orderkey 
-val x101 = x99.c_name 
-val x102 = Record145(x100, x101) 
-x102 
-} 
-val resultInner__D_1 = x103
-val x104 = resultInner__D_1
-//resultInner__D_1.collect.foreach(println(_))
-/**
-M_flat1 :=  REDUCE[ (s_name := x77.s_name,customers2 := (s__Fs_suppkey := x77.s_suppkey)) / true ](S__D._1)
-**/
-val x105 = S__D_1 
-val x111 = x105.map{ case x106 => 
-   val x107 = x106.s_name 
-val x108 = x106.s_suppkey 
-val x109 = Record146(x108) 
-val x110 = Record147(x107, x109) 
-x110 
-} 
-val M_flat1 = x111
-val x112 = M_flat1
-//M_flat1.collect.foreach(println(_))
-/**
-M_flat2 :=  REDUCE[ (key := (s__Fs_suppkey := x78.l_suppkey),value := x80) / true ]( <-- (x78,x80) -- NEST[ U / (c_name2 := x79.c_name) / (x78), true / (x79) ]( <-- (x78,x79) -- (
- <-- (x78) -- SELECT[ true, (l_suppkey := x78.l_suppkey,l_orderkey := x78.l_orderkey) ](L__D._1)) JOIN[x78.l_orderkey = x79.o_orderkey](
-
-   <-- (x79) -- SELECT[ true, x79 ](resultInner__D._1))))
-**/
-val x113 = L__D_1 
-val x118 = x113.map(x114 => { val x115 = x114.l_suppkey 
-val x116 = x114.l_orderkey 
-val x117 = Record148(x115, x116) 
-x117 }) 
-val x119 = resultInner__D_1 
-val x121 = x119 
-val x126 = { val out1 = x118.map{ case x122 => ({val x124 = x122.l_orderkey 
-x124}, x122) }
-  val out2 = x121.map{ case x123 => ({val x125 = x123.o_orderkey 
+ val x117 = L__D_1.map(x113 => { val x114 = x113.l_suppkey 
+val x115 = x113.l_orderkey 
+val x116 = Record164(x114, x115) 
+x116 }) 
+val x122 = O__D_1.map(x118 => { val x119 = x118.o_custkey 
+val x120 = x118.o_orderkey 
+val x121 = Record165(x119, x120) 
+x121 }) 
+val x127 = { val out1 = x117.map{ case x123 => ({val x125 = x123.l_orderkey 
 x125}, x123) }
+  val out2 = x122.map{ case x124 => ({val x126 = x124.o_orderkey 
+x126}, x124) }
   out1.join(out2).map{ case (k,v) => v }
 } 
-val x134 = x126.flatMap{ case (x127, x128) => val x133 = (x128) 
-x133 match {
-   case (null) => Nil 
-   case x132 => List(({val x129 = (x127) 
-x129}, {val x130 = x128.c_name 
-val x131 = Record150(x130) 
-x131}))
- }
-}.groupByLabel() 
-val x140 = x134.map{ case (x135, x136) => 
-   val x137 = x135.l_suppkey 
-val x138 = Record146(x137) 
-val x139 = (x138, x136) 
-x139 
+val x132 = C__D_1.map(x128 => { val x129 = x128.c_name 
+val x130 = x128.c_custkey 
+val x131 = Record166(x129, x130) 
+x131 }) 
+val x138 = { val out1 = x127.map{ case (x133, x134) => ({val x136 = x134.o_custkey 
+x136}, (x133, x134)) }
+  val out2 = x132.map{ case x135 => ({val x137 = x135.c_custkey 
+x137}, x135) }
+  out1.joinSkewLeft(out2).map{ case (k,v) => v }
 } 
-val M_flat2 = x140
-val x141 = M_flat2
+val x145 = x138.map{ case ((x139, x140), x141) => 
+   val x142 = x139.l_suppkey 
+val x143 = x141.c_name 
+val x144 = Record167(x142, x143) 
+x144 
+} 
+val resultInner__D_1 = x145
+val x146 = resultInner__D_1
+//resultInner__D_1.collect.foreach(println(_))
+val x152 = S__D_1.map{ case x147 => 
+   val x148 = x147.s_name 
+val x149 = x147.s_suppkey 
+val x150 = Record168(x149) 
+val x151 = Record169(x148, x150) 
+x151 
+} 
+val M_flat1 = x152
+val x153 = M_flat1
+//M_flat1.collect.foreach(println(_))
+val x161 = resultInner__D_1.map{ case x154 => 
+   val x155 = x154.l_suppkey 
+val x156 = Record168(x155) 
+val x157 = x154.c_name 
+val x158 = Record170(x157) 
+val x159 = x158//List(x158) 
+val x160 = (x156, x159) 
+x160 
+}.groupByLabel() 
+val M_flat2 = x161
+val x162 = M_flat2
 //M_flat2.collect.foreach(println(_))
-x141.count
+x162.count
 }
 var start0 = System.currentTimeMillis()
 f
