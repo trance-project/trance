f2a0986b28cae98610526868ee1130dbc364f4c6
Jaclyn Smith <jacalacka@gmail.com>
2019-12-29 15:12:21 +0000

    query 4 updated nested to nested manual

--- experiments/tpch/Query4SparkManual.scala
+++ experiments/tpch/Query4SparkManual.scala
@@ -28,24 +28,26 @@ object Query4SparkManual {
     P.cache
     P.count
 
-    val o = O.map(o => o.o_custkey -> (o.o_orderkey, o.o_orderdate))
-    val c = C.map(c => c.c_custkey -> c.c_name).join(o).map{ case (_, (c_name, orders)) => (c_name, orders) }.groupByKey()
-    c.cache
-    c.count
-    var start0 = System.currentTimeMillis()
-
     val l = L.map(l => l.l_partkey -> (l.l_orderkey, l.l_quantity))
     val p = P.map(p => p.p_partkey -> p.p_name)
-    val lpj = l.joinSkewLeft(p).map{ case (_, ((lorderkey, lqty), pname)) => lorderkey -> (pname, lqty) }
+    val lpj = l.joinSkewLeft(p)
+
+    val OrderParts = lpj.map{ case (_, ((l_orderkey, l_quantity), p_name)) => (l_orderkey, (p_name, l_quantity)) }.groupByKey()
+    val o = O.map(o => o.o_orderkey -> (o.o_custkey, o.o_orderdate)).join(OrderParts)
+
+    val CustomerOrders = o.map{ case (_, ((o_custkey, o_orderdate), parts)) => (o_custkey, (o_orderdate, parts)) }.groupByKey()
+    val c = C.map(c => c.c_custkey -> c.c_name).join(CustomerOrders).map{ case (_, (c_name, orders)) => (c_name, orders) }
+    c.cache
+    c.count
+
+    var start0 = System.currentTimeMillis()
 
     val custords = c.flatMap{ case (cname, orders) => 
-                    orders.map{ case (orderkey, orderdate) => orderkey -> (cname, orderdate) } 
-                  }.join(lpj)
-                    .map{ case (_, ((cname, orderdate), (pname, qty))) => 
-                      ((cname, orderdate, pname), qty)
-                   }.reduceByKey(_ + _).map{
-                      case ((cname, orderdate, pname), qty) => (cname, (orderdate, pname, qty))
-                   }.groupByKey()  
+                    orders.flatMap{ case (orderdate, parts) => parts.map{
+                      case (pname, lqty) => ((cname, orderdate, pname), lqty)
+                   }}}.reduceByKey(_+_).map{
+                    case ((cname, orderdate, pname), qty) => (cname, (orderdate, pname, qty))
+                   }.groupByKey() 
     custords.count
     var end0 = System.currentTimeMillis() - start0
     println("Query4SparkManual"+sf+","+Config.datapath+","+end0+","+spark.sparkContext.applicationId)
