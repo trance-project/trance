b57cc7a61d65c56dab1a83b04c897e18c3f2c826
Jaclyn Smith <jacalacka@gmail.com>
2020-02-03 13:38:38 +0000

    use case classes in manual query to minimize serializatin effects

--- experiments/skewfix/Query1SparkManual.scala
+++ experiments/skewfix/Query1SparkManual.scala
@@ -8,9 +8,22 @@ import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
 import sprkloader._
 import sprkloader.SkewPairRDD._
-
+/** Inherit same case class from generated code **/
+case class Record165(lbl: Unit)
+case class Record166(l_orderkey: Int, l_quantity: Double, l_partkey: Int)
 case class Record167(p_name: String, p_partkey: Int)
-
+case class Record168(l_orderkey: Int, p_name: String, l_qty: Double)
+case class Record169(c_name: String, c_custkey: Int)
+case class Record170(c__Fc_custkey: Int)
+case class Record171(c_name: String, c_orders: Record170)
+case class Record172(lbl: Record170)
+case class Record173(o_orderdate: String, o_orderkey: Int, o_custkey: Int)
+case class Record175(o__Fo_orderkey: Int)
+case class Record176(o_orderdate: String, o_parts: Record175)
+case class Record177(lbl: Record175)
+case class Record179(p_name: String, l_qty: Double)
+case class Record232(o_orderdate: String, o_parts: Iterable[Record179])
+case class Record233(c_name: String, c_orders: Iterable[Record232])
 object Query1SparkManual {
   def main(args: Array[String]){
     val sf = Config.datapath.split("/").last
@@ -18,20 +31,20 @@ object Query1SparkManual {
     val spark = SparkSession.builder().config(conf).getOrCreate()
 
     val tpch = TPCHLoader(spark)
-    val C = tpch.loadCustomers
+    val C = tpch.loadCustomersProj
     C.cache
     spark.sparkContext.runJob(C, (iter: Iterator[_]) => {})
-    val O = tpch.loadOrders
+    val O = tpch.loadOrdersProj
     O.cache
     spark.sparkContext.runJob(O, (iter: Iterator[_]) => {})
-    val L = tpch.loadLineitem
+    val L = tpch.loadLineitemProj
     L.cache
     spark.sparkContext.runJob(L, (iter: Iterator[_]) => {})
-    val P = tpch.loadPart
+    val P = tpch.loadPartProj
     P.cache
     spark.sparkContext.runJob(P, (iter: Iterator[_]) => {})
        
-	tpch.triggerGC
+	  tpch.triggerGC
 
     var start0 = System.currentTimeMillis()
 
@@ -39,15 +52,16 @@ object Query1SparkManual {
     val p = P.map(p => p.p_partkey -> Record167(p.p_name, p.p_partkey))
     val lpj = l.joinSkewLeft(p)
 
-    val OrderParts = lpj.map{ case (_, ((l_orderkey, l_quantity), p_name)) => l_orderkey -> (p_name, l_quantity) }
-    val CustomerOrders = O.map(o => o.o_orderkey -> (o.o_custkey, o.o_orderdate)).cogroup(OrderParts).flatMap{
-		  case (_, (order, parts)) => order.map{ case (ock, od) => ock -> (od, parts.toArray) }
+    val OrderParts = lpj.map{ case (l, p) => l.l_orderkey -> Record179(p.p_name, l.l_quantity) }
+    val CustomerOrders = O.map(o => o.o_orderkey -> Record173(o.o_orderdate, o.o_orderkey, o.o_custkey)).cogroup(OrderParts).flatMap{
+		  case (_, (orders, parts)) => orders.map(order => order.o_custkey -> Record232(order.o_orderdate, parts))
 	  }
 
-    val c = C.map(c => c.c_custkey -> c.c_name).cogroup(CustomerOrders).flatMap{ 
-	  			case (_, (c_name, orders)) => c_name.map(c => (c, orders.toArray)) 
+    val c = C.map(c => c.c_custkey -> Record169(c.c_name, c.c_custkey)).cogroup(CustomerOrders).flatMap{ 
+	  			case (_, (cnames, orders)) => cnames.map(c => Record233(c.c_name, orders)) 
 			}
     spark.sparkContext.runJob(c, (iter: Iterator[_]) => {})
+    //c.collect.foreach(println(_))
     var end0 = System.currentTimeMillis() - start0
     println("Query1SparkManual"+sf+","+Config.datapath+","+end0+",query,"+spark.sparkContext.applicationId)
   }
