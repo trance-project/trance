1bec1ad87d27779c834780f6ed800af9c043db77
Jaclyn Smith <jacalacka@gmail.com>
2020-02-04 14:33:31 +0000

    separating out pair rdd functions

--- spark/src/main/scala/sprkloader/SkewPairRDD.scala
+++ spark/src/main/scala/sprkloader/SkewPairRDD.scala
@@ -6,72 +6,14 @@ import org.apache.spark.broadcast.Broadcast
 import scala.reflect.ClassTag
 import org.apache.spark.Partitioner
 import UtilPairRDD._
+import UtilSkewDistribution._
 
 object SkewPairRDD {
   
-  implicit class SkewDictFunctions[K: ClassTag, V: ClassTag](lrdd: RDD[(K,Iterable[V])]) extends Serializable {
-
-    val reducers = Config.minPartitions 
-
-    def heavyKeys(threshold: Int = 1000): Set[K] = {
-      val hkeys = lrdd.mapPartitions( it => 
-        it.foldLeft(HashMap.empty[K, Int].withDefaultValue(0))((acc, c) =>
-          { acc(c._1) += c._2.size; acc } ) .filter(_._2 > threshold).iterator,
-      true).reduceByKey(_ + _)
-      
-      if (reducers > threshold) hkeys.filter(_._2 >= reducers).keys.collect.toSet
-      else hkeys.keys.collect.toSet
-    }
- 
- 	def balanceLeft[S](rrdd: RDD[(K, S)], hkeys: Broadcast[Set[K]]): (RDD[((K, Int), Iterable[V])], RDD[((K, Int), S)]) = {
-      val lrekey = lrdd.mapPartitionsWithIndex( (index, it) =>
-        it.zipWithIndex.map{ case ((k,v), i) => 
-          (k, { if (hkeys.value(k)) i % reducers else 0 }) -> v
-        }, true)
-      val rdupp = rrdd.flatMap{ case (k,v) =>
-        Range(0, {if (hkeys.value(k)) reducers else 1 }).map(id => (k, id) -> v) 
-      }
-      (lrekey, rdupp)
-    }
-
-    def lookup[S](rrdd: RDD[(K,S)]): RDD[(S, Iterable[V])] = {
-      lrdd.cogroup(rrdd).flatMap{ pair =>
-        for (b <- pair._2._1.iterator; l <- pair._2._2.iterator) yield (l, b)
-      }
-    }
-
-    def lookupSkewLeft[S](rrdd: RDD[(K, S)]): RDD[(S, Iterable[V])] = {
-      val hk = heavyKeys()
-      if (hk.nonEmpty) {
-        val hkeys = lrdd.sparkContext.broadcast(hk)
-        val (rekey,dupp) = lrdd.balanceLeft(rrdd, hkeys)
-	 	rekey.cogroup(dupp).flatMap{ pair =>
-          for (b <- pair._2._1.iterator; l <- pair._2._2.iterator) yield (l, b)
-        }
-      } else lrdd.lookup(rrdd) 
-    }
-
-  }
-
-  // ((key, partitionIndex), count)
-  implicit class SkewDistributionFunctions[K: ClassTag](mrdd: RDD[((K, Int), Int)]) {
-	def getKeys():Set[K] = mrdd.map{ case ((key, pid), cnt) => key }.collect.toSet
-	def isBalanced(threshold: Double = 0.95): Boolean = {
-		// this is wrong should be counts by key
-		val cnts = mrdd.map{ case ((key, pid), cnt) => cnt }
-		val min = cnts.min
-		val max = cnts.max
-		val skew = min/max.toDouble
-		println(s"found this skew $skew $min $max")
-		true
-		//skew >= threshold
-	}
-  }
-
   implicit class SkewPairRDDFunctions[K: ClassTag, V: ClassTag](lrdd: RDD[(K,V)]) extends Serializable {
 
     val reducers = Config.minPartitions
-	val partitions = lrdd.getNumPartitions
+	  val partitions = lrdd.getNumPartitions
 
     def heavyKeys(threshold: Int = 1000): Set[K] = {
       val hkeys = lrdd.mapPartitions( it => 
@@ -159,15 +101,15 @@ object SkewPairRDD {
 			val hk = partCnts.getKeys()
 			(partCnts.isBalanced() && hk.nonEmpty, hk)
 		}else{
-			val hk = heavyKeys()
+			val hk = heavyKeys(0)
 			(hk.nonEmpty, hk)
 		} 
-	  	if (skewedKeys) {
+	  if (skewedKeys) {
        		val hkeys = lrdd.sparkContext.broadcast(hk)
        		val (rekey, dupp) = lrdd.rekeyByIndex(rrdd, hkeys)
        		rekey.joinDropKey(dupp, Some(new SkewPartitioner(partitions)))
 		}else joinDropKey(rrdd)
-    }
+  }
 
 	def joinSkewLeftPrintDist[S](rrdd: RDD[(K, S)]): RDD[(V, S)] = { 
       val hkm = lrdd.heavyKeysByPartition[K]()
---
+++ spark/src/main/scala/sprkloader/UtilSkewDistribution.scala
@@ -0,0 +1,29 @@
+package sprkloader
+
+import scala.collection.mutable.HashMap
+import org.apache.spark.rdd.RDD
+import org.apache.spark.broadcast.Broadcast
+import scala.reflect.ClassTag
+import org.apache.spark.Partitioner
+import UtilPairRDD._
+
+object UtilSkewDistribution {
+
+  // ((key, partitionIndex), count)
+  implicit class SkewDistributionFunctions[K: ClassTag](mrdd: RDD[((K, Int), Int)]) {
+  
+    def getKeys():Set[K] = mrdd.map{ case ((key, pid), cnt) => key }.collect.toSet
+  
+    def isBalanced(threshold: Double = 0.95): Boolean = {
+      // this is wrong should be counts by key
+      val cnts = mrdd.map{ case ((key, pid), cnt) => cnt }
+      val min = cnts.min
+      val max = cnts.max
+      val skew = min/max.toDouble
+      println(s"found this skew $skew $min $max")
+      true
+      //skew >= threshold
+    }
+  }
+
+}
