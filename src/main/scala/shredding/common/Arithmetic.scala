27624b9f449cb888d357c2eacd72fefb67c2d739
Milos Nikolic <milos.nikolic83@gmail.com>
2020-03-11 17:58:50 +0000

    Minor reformatting

--- src/main/scala/shredding/common/Arithmetic.scala
+++ src/main/scala/shredding/common/Arithmetic.scala
@@ -3,9 +3,15 @@ package shredding.core
 /**
   * Arithmetic operators
   */
+
 sealed trait OpArithmetic
+
 case object OpPlus extends OpArithmetic { override def toString = "+" }
+
 case object OpMinus extends OpArithmetic { override def toString = "-" }
+
 case object OpMultiply extends OpArithmetic { override def toString = "*" }
+
 case object OpDivide extends OpArithmetic { override def toString = "/" }
+
 case object OpMod extends OpArithmetic { override def toString = "mod" }
