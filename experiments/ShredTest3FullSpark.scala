c90666771f05ca3fa6cd83f892abb2164db12828
Jaclyn Smith <jacalacka@gmail.com>
2020-03-25 20:39:19 +0000

    exploring peak in unshredding

--- experiments/ShredTest3FullSpark.scala
+++ experiments/ShredTest3FullSpark.scala
@@ -7,20 +7,22 @@ import sprkloader._
 import sprkloader.PairRDDOperations._
 import sprkloader.DictRDDOperations._
 import sprkloader.TopRDD._
-import org.apache.spark.HashPartitioner
-case class Record4481(n__F_n_nationkey: Int)
-case class Record4482(n_regionkey: Int, n_nationkey: Int, n_custs: Record4481, n_name: String, n_comment: String)
-case class Record4484(c__F_c_custkey: Int)
-case class Record4485(c_acctbal: Double, c_name: String, c_nationkey: Int, c_custkey: Int, c_comment: String, c_address: String, c_orders: Record4484, c_mktsegment: String, c_phone: String)
-case class Record4487(o__F_o_orderkey: Int)
-case class Record4488(o_shippriority: Int, o_orderdate: String, o_custkey: Int, o_orderpriority: String, o_parts: Record4487, o_clerk: String, o_orderstatus: String, o_totalprice: Double, o_orderkey: Int, o_comment: String)
-case class Record4490(l_returnflag: String, l_comment: String, l_linestatus: String, l_shipmode: String, l_shipinstruct: String, l_quantity: Double, l_receiptdate: String, l_linenumber: Int, l_tax: Double, l_shipdate: String, l_extendedprice: Double, l_partkey: Int, l_discount: Double, l_commitdate: String, l_suppkey: Int, l_orderkey: Int)
-case class Record4491(o_shippriority: Int, o_orderdate: String, o_custkey: Int, o_orderpriority: String, o_clerk: String, o_orderstatus: String, o_totalprice: Double, o_orderkey: Int, o_comment: String)
-case class Record4492(o_shippriority: Int, o_orderdate: String, o_custkey: Int, o_orderpriority: String, o_parts: Vector[Record4490], o_clerk: String, o_orderstatus: String, o_totalprice: Double, o_orderkey: Int, o_comment: String)
-case class Record4493(c_acctbal: Double, c_name: String, c_nationkey: Int, c_custkey: Int, c_comment: String, c_address: String, c_mktsegment: String, c_phone: String)
-case class Record4494(c_acctbal: Double, c_name: String, c_nationkey: Int, c_custkey: Int, c_comment: String, c_address: String, c_orders: Vector[Record4492], c_mktsegment: String, c_phone: String)
-case class Record4496(n_regionkey: Int, n_nationkey: Int, n_name: String, n_comment: String)
-case class Record4497(n_regionkey: Int, n_nationkey: Int, n_custs: Vector[Record4494], n_name: String, n_comment: String)
+case class Record163(n__F_n_nationkey: Int)
+case class Record164(n_regionkey: Int, n_nationkey: Int, n_custs: Record163, n_name: String, n_comment: String)
+case class Record165(c__F_c_custkey: Int)
+case class Record166(c_acctbal: Double, c_name: String, c_nationkey: Int, _1: Int, c_custkey: Int, c_comment: String, c_address: String, c_orders: Int, c_mktsegment: String, c_phone: String)
+case class Record167(o__F_o_orderkey: Int)
+case class Record168(o_shippriority: Int, o_orderdate: String, o_custkey: Int, o_orderpriority: String, o_parts: Record167, _1: Int, o_clerk: String, o_orderstatus: String, o_totalprice: Double, o_orderkey: Int, o_comment: String)
+case class Record169(l_returnflag: String, l_comment: String, l_linestatus: String, l_shipmode: String, l_shipinstruct: String, l_quantity: Double, l_receiptdate: String, l_linenumber: Int, l_tax: Double, l_shipdate: String, _1: Int, l_extendedprice: Double, l_partkey: Int, l_discount: Double, l_commitdate: String, l_suppkey: Int, l_orderkey: Int)
+case class Record172(o_shippriority: Int, o_orderdate: String, o_custkey: Int, o_orderpriority: String, _1: Int, o_clerk: String, o_orderstatus: String, o_totalprice: Double, o_orderkey: Int, o_comment: String)
+case class Record173(l_returnflag: String, l_comment: String, l_linestatus: String, l_shipmode: String, l_shipinstruct: String, l_quantity: Double, l_receiptdate: String, l_linenumber: Int, l_tax: Double, l_shipdate: String, l_extendedprice: Double, l_partkey: Int, l_discount: Double, l_commitdate: String, l_suppkey: Int, l_orderkey: Int)
+case class Record174(o_shippriority: Int, o_orderdate: String, o_custkey: Int, o_orderpriority: String, o_parts: Vector[Record173], _1: Int, o_clerk: String, o_orderstatus: String, o_totalprice: Double, o_orderkey: Int, o_comment: String)
+case class Record177(c_acctbal: Double, c_name: String, c_nationkey: Int, _1: Int, c_custkey: Int, c_comment: String, c_address: String, c_mktsegment: String, c_phone: String)
+case class Record178(o_shippriority: Int, o_orderdate: String, o_custkey: Int, o_orderpriority: String, o_parts: Vector[Record173], o_clerk: String, o_orderstatus: String, o_totalprice: Double, o_orderkey: Int, o_comment: String)
+case class Record179(c_acctbal: Double, c_name: String, c_nationkey: Int, _1: Int, c_custkey: Int, c_comment: String, c_address: String, c_orders: Vector[Record178], c_mktsegment: String, c_phone: String)
+case class Record182(n_regionkey: Int, n_nationkey: Int, n_name: String, n_comment: String)
+case class Record183(c_acctbal: Double, c_name: String, c_nationkey: Int, c_custkey: Int, c_comment: String, c_address: String, c_orders: Vector[Record178], c_mktsegment: String, c_phone: String)
+case class Record184(n_regionkey: Int, n_nationkey: Int, n_custs: Vector[Record183], n_name: String, n_comment: String)
 object ShredTest3FullSpark {
  def main(args: Array[String]){
    val sf = Config.datapath.split("/").last
@@ -43,170 +45,170 @@ spark.sparkContext.runJob(IBag_O__D, (iter: Iterator[_]) => {})
     def f = {
  
 var start0 = System.currentTimeMillis()
-val x4311 = IBag_N__D.map{ case x4304 => 
-   {val x4305 = x4304.n_regionkey 
-val x4306 = x4304.n_nationkey 
-val x4307 = Record4481(x4306) 
-val x4308 = x4304.n_name 
-val x4309 = x4304.n_comment 
-val x4310 = Record4482(x4305, x4306, x4307, x4308, x4309) 
-x4310}
-} 
-val MBag_Test3Full_1 = x4311
-val x4312 = MBag_Test3Full_1
-MBag_Test3Full_1.cache
+val x19 = IBag_N__D
+// .map{ case x12 => 
+//    {val x13 = x12.n_regionkey 
+// val x14 = x12.n_nationkey 
+// val x15 = Record163(x14) 
+// val x16 = x12.n_name 
+// val x17 = x12.n_comment 
+// val x18 = Record164(x13, x14, x15, x16, x17) 
+// x18}
+// } 
+val MBag_Test3Full_1 = x19
+val x20 = MBag_Test3Full_1
+//MBag_Test3Full_1.cache
 //MBag_Test3Full_1.print
-MBag_Test3Full_1.evaluate
-val x4314 = IBag_C__D 
-val x4330 = x4314.map{ 
-  case x4315 => ({val x4316 = x4315.c_nationkey 
-val x4317 = Record4481(x4316) 
-x4317}, {val x4318 = x4315.c_acctbal 
-val x4319 = x4315.c_name 
-val x4320 = x4315.c_nationkey 
-val x4321 = x4315.c_custkey 
-val x4322 = x4315.c_comment 
-val x4323 = x4315.c_address 
-val x4324 = Record4484(x4321) 
-val x4325 = x4315.c_mktsegment 
-val x4326 = x4315.c_phone 
-val x4327 = Record4485(x4318, x4319, x4320, x4321, x4322, x4323, x4324, x4325, x4326) 
-x4327})
-}.partitionBy(new HashPartitioner(400))//.group(_++_) 
-val MDict_Test3Full_1_n_custs_1 = x4330
-val x4331 = MDict_Test3Full_1_n_custs_1
-MDict_Test3Full_1_n_custs_1.cache
+// MBag_Test3Full_1.evaluate
+val x22 = IBag_C__D 
+// val x35 = x22.map{ case x23 => 
+//    {val x24 = x23.c_acctbal 
+// val x25 = x23.c_name 
+// val x26 = x23.c_nationkey 
+// val x27 = Record163(x26) 
+// val x28 = x23.c_custkey 
+// val x29 = x23.c_comment 
+// val x30 = x23.c_address 
+// val x31 = Record165(x28) 
+// val x32 = x23.c_mktsegment 
+// val x33 = x23.c_phone 
+// val x34 = Record166(x24, x25, x26, x27, x28, x29, x30, x31, x32, x33) 
+// x34}
+// } 
+// val x36 = x35 
+val MDict_Test3Full_1_n_custs_1 = x22//x36
+val x37 = MDict_Test3Full_1_n_custs_1
+//MDict_Test3Full_1_n_custs_1.cache
 //MDict_Test3Full_1_n_custs_1.print
-MDict_Test3Full_1_n_custs_1.evaluate
-val x4333 = IBag_O__D 
-val x4350 = x4333.map{ 
-  case x4334 => ({val x4335 = x4334.o_custkey 
-val x4336 = Record4484(x4335) 
-x4336}, {val x4337 = x4334.o_shippriority 
-val x4338 = x4334.o_orderdate 
-val x4339 = x4334.o_custkey 
-val x4340 = x4334.o_orderpriority 
-val x4341 = x4334.o_orderkey 
-val x4342 = Record4487(x4341) 
-val x4343 = x4334.o_clerk 
-val x4344 = x4334.o_orderstatus 
-val x4345 = x4334.o_totalprice 
-val x4346 = x4334.o_comment 
-val x4347 = Record4488(x4337, x4338, x4339, x4340, x4342, x4343, x4344, x4345, x4341, x4346) 
-x4347})
-}.partitionBy(new HashPartitioner(500))//.group(_++_) 
-val MDict_Test3Full_1_n_custs_1_c_orders_1 = x4350
-val x4351 = MDict_Test3Full_1_n_custs_1_c_orders_1
-MDict_Test3Full_1_n_custs_1_c_orders_1.cache
+// MDict_Test3Full_1_n_custs_1.evaluate
+val x39 = IBag_O__D 
+// val x53 = x39.map{ case x40 => 
+//    {val x41 = x40.o_shippriority 
+// val x42 = x40.o_orderdate 
+// val x43 = x40.o_custkey 
+// val x44 = x40.o_orderpriority 
+// val x45 = x40.o_orderkey 
+// val x46 = Record167(x45) 
+// val x47 = Record165(x43) 
+// val x48 = x40.o_clerk 
+// val x49 = x40.o_orderstatus 
+// val x50 = x40.o_totalprice 
+// val x51 = x40.o_comment 
+// val x52 = Record168(x41, x42, x43, x44, x46, x47, x48, x49, x50, x45, x51) 
+// x52}
+// } 
+// val x54 = x53 
+val MDict_Test3Full_1_n_custs_1_c_orders_1 = x39//x54
+val x55 = MDict_Test3Full_1_n_custs_1_c_orders_1
+//MDict_Test3Full_1_n_custs_1_c_orders_1.cache
 //MDict_Test3Full_1_n_custs_1_c_orders_1.print
-MDict_Test3Full_1_n_custs_1_c_orders_1.evaluate
-val x4353 = IBag_L__D 
-val x4376 = x4353.map{ 
-  case x4354 => ({val x4355 = x4354.l_orderkey 
-val x4356 = Record4487(x4355) 
-x4356}, {val x4357 = x4354.l_returnflag 
-val x4358 = x4354.l_comment 
-val x4359 = x4354.l_linestatus 
-val x4360 = x4354.l_shipmode 
-val x4361 = x4354.l_shipinstruct 
-val x4362 = x4354.l_quantity 
-val x4363 = x4354.l_receiptdate 
-val x4364 = x4354.l_linenumber 
-val x4365 = x4354.l_tax 
-val x4366 = x4354.l_shipdate 
-val x4367 = x4354.l_extendedprice 
-val x4368 = x4354.l_partkey 
-val x4369 = x4354.l_discount 
-val x4370 = x4354.l_commitdate 
-val x4371 = x4354.l_suppkey 
-val x4372 = x4354.l_orderkey 
-val x4373 = Record4490(x4357, x4358, x4359, x4360, x4361, x4362, x4363, x4364, x4365, x4366, x4367, x4368, x4369, x4370, x4371, x4372) 
-x4373})
-}.partitionBy(new HashPartitioner(1000))//.group(_++_) 
-val MDict_Test3Full_1_n_custs_1_c_orders_1_o_parts_1 = x4376
-val x4377 = MDict_Test3Full_1_n_custs_1_c_orders_1_o_parts_1
-MDict_Test3Full_1_n_custs_1_c_orders_1_o_parts_1.cache
+// MDict_Test3Full_1_n_custs_1_c_orders_1.evaluate
+val x57 = IBag_L__D 
+// val x77 = x57.map{ case x58 => 
+//    {val x59 = x58.l_returnflag 
+// val x60 = x58.l_comment 
+// val x61 = x58.l_linestatus 
+// val x62 = x58.l_shipmode 
+// val x63 = x58.l_shipinstruct 
+// val x64 = x58.l_quantity 
+// val x65 = x58.l_receiptdate 
+// val x66 = x58.l_linenumber 
+// val x67 = x58.l_tax 
+// val x68 = x58.l_shipdate 
+// val x69 = x58.l_orderkey 
+// val x70 = Record167(x69) 
+// val x71 = x58.l_extendedprice 
+// val x72 = x58.l_partkey 
+// val x73 = x58.l_discount 
+// val x74 = x58.l_commitdate 
+// val x75 = x58.l_suppkey 
+// val x76 = Record169(x59, x60, x61, x62, x63, x64, x65, x66, x67, x68, x70, x71, x72, x73, x74, x75, x69) 
+// x76}
+// } 
+// val x78 = x77 
+val MDict_Test3Full_1_n_custs_1_c_orders_1_o_parts_1 = x57//x78
+val x79 = MDict_Test3Full_1_n_custs_1_c_orders_1_o_parts_1
+//MDict_Test3Full_1_n_custs_1_c_orders_1_o_parts_1.cache
 //MDict_Test3Full_1_n_custs_1_c_orders_1_o_parts_1.print
-MDict_Test3Full_1_n_custs_1_c_orders_1_o_parts_1.evaluate
+// MDict_Test3Full_1_n_custs_1_c_orders_1_o_parts_1.evaluate
 var end0 = System.currentTimeMillis() - start0
 println("Shred,3,"+sf+","+Config.datapath+","+end0+",query,"+spark.sparkContext.applicationId)
-
-IBag_L__D.unpersist()
-IBag_O__D.unpersist()
-IBag_C__D.unpersist()
-IBag_N__D.unpersist()
-
-tpch.triggerGC
+    
 
 var start1 = System.currentTimeMillis()
-val x4413 = MDict_Test3Full_1_n_custs_1_c_orders_1 
-val x4417 = x4413.map{case (x4414, x4416) => 
-   (x4416.o_parts, (x4414, Record4491(x4416.o_shippriority, x4416.o_orderdate, x4416.o_custkey, x4416.o_orderpriority, 
-    x4416.o_clerk, x4416.o_orderstatus, x4416.o_totalprice, x4416.o_orderkey, x4416.o_comment)))
-}
-val x4422 = x4417.cogroupDropKey(MDict_Test3Full_1_n_custs_1_c_orders_1_o_parts_1)//.rightCoGroupDropKey(x4417)
-val x4437 = x4422.map{ case ((x4423, x4424), x4425) => 
-   {val x4426 = x4424.o_shippriority 
-val x4427 = x4424.o_orderdate 
-val x4428 = x4424.o_custkey 
-val x4429 = x4424.o_orderpriority 
-val x4430 = x4424.o_clerk 
-val x4431 = x4424.o_orderstatus 
-val x4432 = x4424.o_totalprice 
-val x4433 = x4424.o_orderkey 
-val x4434 = x4424.o_comment 
-val x4435 = Record4492(x4426, x4427, x4428, x4429, x4425, x4430, x4431, x4432, x4433, x4434) 
-val x4436 = (x4423,x4435) 
-x4436}
+val x170 = MDict_Test3Full_1_n_custs_1_c_orders_1.map{ case x105 => (x105.o_orderkey, Record172(x105.o_shippriority, x105.o_orderdate, x105.o_custkey, x105.o_orderpriority, x105.o_custkey, x105.o_clerk, x105.o_orderstatus, x105.o_totalprice, x105.o_orderkey, x105.o_comment))}
+val x171 = MDict_Test3Full_1_n_custs_1_c_orders_1_o_parts_1.map{ x106 => (x106.l_orderkey, Record173(x106.l_returnflag, x106.l_comment, x106.l_linestatus, x106.l_shipmode, x106.l_shipinstruct, x106.l_quantity, x106.l_receiptdate, x106.l_linenumber, x106.l_tax, x106.l_shipdate, x106.l_extendedprice, x106.l_partkey, x106.l_discount, x106.l_commitdate, x106.l_suppkey, x106.l_orderkey)) }
+val x108 = x170.cogroupDropKey(x171)
+val x122 = x108.map{ case (x109, x110) => 
+   {val x111 = x109.o_shippriority 
+val x112 = x109.o_orderdate 
+val x113 = x109.o_custkey 
+val x114 = x109.o_orderpriority 
+val x115 = x109._1 
+val x116 = x109.o_clerk 
+val x117 = x109.o_orderstatus 
+val x118 = x109.o_totalprice 
+val x119 = x109.o_orderkey 
+val x120 = x109.o_comment 
+val x121 = Record174(x111, x112, x113, x114, x110, x115, x116, x117, x118, x119, x120) 
+x121}
 } 
-val UDict_Test3Full_1_n_custs_1_c_orders_1 = x4437
-val x4438 = UDict_Test3Full_1_n_custs_1_c_orders_1
+val x123 = x122 
+val UDict_Test3Full_1_n_custs_1_c_orders_1 = x123
+val x124 = UDict_Test3Full_1_n_custs_1_c_orders_1
 //UDict_Test3Full_1_n_custs_1_c_orders_1.cache
 //UDict_Test3Full_1_n_custs_1_c_orders_1.print
 // UDict_Test3Full_1_n_custs_1_c_orders_1.evaluate
-val x4440 = MDict_Test3Full_1_n_custs_1 
-val x4444 = x4440.map{case (x4441, x4443) => 
-   (x4443.c_orders, (x4441, Record4493(x4443.c_acctbal, x4443.c_name, x4443.c_nationkey, x4443.c_custkey, x4443.c_comment, 
-    x4443.c_address, x4443.c_mktsegment, x4443.c_phone)))
-}
-val x4449 = x4444.cogroupDropKey(UDict_Test3Full_1_n_custs_1_c_orders_1)//.rightCoGroupDropKey(x4444)
-val x4463 = x4449.map{ case ((x4450, x4451), x4452) => 
-   {val x4453 = x4451.c_acctbal 
-val x4454 = x4451.c_name 
-val x4455 = x4451.c_nationkey 
-val x4456 = x4451.c_custkey 
-val x4457 = x4451.c_comment 
-val x4458 = x4451.c_address 
-val x4459 = x4451.c_mktsegment 
-val x4460 = x4451.c_phone 
-val x4461 = Record4494(x4453, x4454, x4455, x4456, x4457, x4458, x4452, x4459, x4460) 
-val x4462 = (x4450,x4461) 
-x4462}
+val x175 = MDict_Test3Full_1_n_custs_1.map{ case x128 => (x128.c_custkey, Record177(x128.c_acctbal, x128.c_name, x128.c_nationkey, x128.c_custkey, x128.c_custkey, x128.c_comment, x128.c_address, x128.c_mktsegment, x128.c_phone))}
+val x176 = UDict_Test3Full_1_n_custs_1_c_orders_1.map{ x129 => (x129.o_custkey, Record178(x129.o_shippriority, x129.o_orderdate, x129.o_custkey, x129.o_orderpriority, x129.o_parts, x129.o_clerk, x129.o_orderstatus, x129.o_totalprice, x129.o_orderkey, x129.o_comment)) }
+val x131 = x175.cogroupDropKey(x176)
+val x144 = x131.map{ case (x132, x133) => 
+   {val x134 = x132.c_acctbal 
+val x135 = x132.c_name 
+val x136 = x132.c_nationkey 
+val x137 = x132._1 
+val x138 = x132.c_custkey 
+val x139 = x132.c_comment 
+val x140 = x132.c_address 
+val x141 = x132.c_mktsegment 
+val x142 = x132.c_phone 
+val x143 = Record179(x134, x135, x136, x137, x138, x139, x140, x133, x141, x142) 
+x143}
 } 
-val UDict_Test3Full_1_n_custs_1 = x4463
-val x4464 = UDict_Test3Full_1_n_custs_1
+val x145 = x144 
+val UDict_Test3Full_1_n_custs_1 = x145
+val x146 = UDict_Test3Full_1_n_custs_1
 //UDict_Test3Full_1_n_custs_1.cache
 //UDict_Test3Full_1_n_custs_1.print
 // UDict_Test3Full_1_n_custs_1.evaluate
-val x4466 = MBag_Test3Full_1 
-val x4495 = x4466.map{ case x4467 => (x4467.n_custs, Record4496(x4467.n_regionkey, x4467.n_nationkey, x4467.n_name, x4467.n_comment))}
-val x4470 = x4495.cogroupDropKey(UDict_Test3Full_1_n_custs_1)//.rightCoGroupDropKey(x4495)
-val x4478 = x4470.map{ case (x4471, x4472) => 
-   {val x4473 = x4471.n_regionkey 
-val x4474 = x4471.n_nationkey 
-val x4475 = x4471.n_name 
-val x4476 = x4471.n_comment 
-val x4477 = Record4497(x4473, x4474, x4472, x4475, x4476) 
-x4477}
+val x180 = MBag_Test3Full_1.map{ case x149 => (x149.n_nationkey, Record182(x149.n_regionkey, x149.n_nationkey, x149.n_name, x149.n_comment))}
+val x181 = UDict_Test3Full_1_n_custs_1.map{ x150 => (x150.c_nationkey, Record183(x150.c_acctbal, x150.c_name, x150.c_nationkey, x150.c_custkey, x150.c_comment, x150.c_address, x150.c_orders, x150.c_mktsegment, x150.c_phone)) }
+val x152 = x180.cogroupDropKey(x181)
+val x160 = x152.map{ case (x153, x154) => 
+   {val x155 = x153.n_regionkey 
+val x156 = x153.n_nationkey 
+val x157 = x153.n_name 
+val x158 = x153.n_comment 
+val x159 = Record184(x155, x156, x154, x157, x158) 
+x159}
 } 
-val Test3Full = x4478
-val x4479 = Test3Full
+val Test3Full = x160
+val x161 = Test3Full
 //Test3Full.cache
 // Test3Full.print
 Test3Full.evaluate
 
-
-
+Test3Full.flatMap{
+      case nations => if (nations.n_custs.isEmpty) Vector((nations.n_name, null, null, -1, -1.0))
+        else nations.n_custs.flatMap{
+          case customers => if (customers.c_orders.isEmpty) Vector((nations.n_name, customers.c_name, null, -1, -1.0))
+            else customers.c_orders.flatMap{
+              case orders => if (orders.o_parts.isEmpty) Vector((nations.n_name, customers.c_name, orders.o_orderdate, -1, -1.0))
+                else orders.o_parts.map{ p => (nations.n_name, customers.c_name, orders.o_orderdate, p.l_partkey, p.l_quantity) }
+                }}}.collect.foreach(println(_))
+        
+        
+        
 var end1 = System.currentTimeMillis() - start1
 println("Shred,3,"+sf+","+Config.datapath+","+end1+",unshredding,"+spark.sparkContext.applicationId)
     
