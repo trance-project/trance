36058959d54267ff5c64d6853a38b4bda5c9ab99
Jaclyn Smith <jacalacka@gmail.com>
2020-04-03 10:35:15 +0100

    adding full dataframe test 2

---
+++ spark/src/main/scala/sprkloader/experiments/ShredQuery1FullSparkDataframe.scala
@@ -0,0 +1,82 @@
+
+package sprkloader.experiments
+/** Generated **/
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
+import scala.collection.mutable.HashMap
+import sprkloader._
+import sprkloader.PairRDDOperations._
+import sprkloader.DictRDDOperations._
+import sprkloader.TopRDD._
+import org.apache.spark.sql._
+
+case class KeyL(_1: Int, o_parts: Seq[Lineitem])
+case class KeyO(_1: Int, c_orders: Seq[TmpO])
+case class TmpO(o_shippriority: Int, o_orderdate: String, o_custkey: Int, o_orderpriority: String, o_clerk: String, o_orderstatus: String, o_totalprice: Double, o_orderkey: Int, o_comment: String, o_parts: Seq[Lineitem])
+case class TmpC(c_acctbal: Double, c_name: String, c_nationkey: Int, c_custkey: Int, c_comment: String, c_address: String, c_mktsegment: String, c_phone: String, c_orders: Seq[Order])
+
+object ShredQuery1FullSparkDataframe extends App {
+ override def main(args: Array[String]){
+   val sf = Config.datapath.split("/").last
+   val conf = new SparkConf().setMaster(Config.master).setAppName("ShredQuery1FullSparkDataframe"+sf)
+   val spark = SparkSession.builder().config(conf).getOrCreate()
+   val tpch = TPCHLoader(spark)
+   import spark.implicits._
+val IBag_L__D = tpch.loadLineitemDF()
+IBag_L__D.cache
+IBag_L__D.count
+// val IBag_P__D = tpch.loadPartDF()
+// IBag_P__D.cache
+// IBag_P__D.count
+val IBag_C__D = tpch.loadCustomersDF()
+IBag_C__D.cache
+IBag_C__D.count
+val IBag_O__D = tpch.loadOrdersDF()
+IBag_O__D.cache
+IBag_O__D.count
+implicit val ncodec = Encoders.product[TmpC]
+implicit val ncode1 = Encoders.product[TmpO]
+implicit val ncode2 = Encoders.product[KeyL]
+implicit val ncode3 = Encoders.product[KeyO]
+
+    def f = {
+ 
+var start0 = System.currentTimeMillis()
+val MBag_Query1_1 = IBag_C__D
+
+val MDict_Query1_1_c_orders_1 = IBag_O__D
+
+val MDict_Query1_1_c_orders_1_o_parts_1 = IBag_L__D
+
+var end0 = System.currentTimeMillis() - start0
+println("Shred,Standard,Query1,"+sf+","+Config.datapath+","+end0+",query,"+spark.sparkContext.applicationId)
+  
+var start1 = System.currentTimeMillis()
+
+val dict1 = MDict_Query1_1_c_orders_1_o_parts_1
+.groupByKey(x => x.l_orderkey).mapGroups{
+  case (lbl, bag) => KeyL(lbl, bag.toSeq)
+}
+
+val dict2 = dict1.join(MDict_Query1_1_c_orders_1, 
+  dict1("_1") === MDict_Query1_1_c_orders_1("o_orderkey")).drop("_1").as[TmpO]
+.groupByKey(x => x.o_custkey).mapGroups{
+  case (lbl, bag) => KeyO(lbl, bag.toSeq)
+}
+
+val result = dict2.join(MBag_Query1_1, MBag_Query1_1("c_custkey") === dict2("_1"))
+  .drop("_1").as[TmpC]
+
+result.count
+
+var end1 = System.currentTimeMillis() - start1
+println("Shred,Standard,Query1,"+sf+","+Config.datapath+","+end1+",unshredding,"+spark.sparkContext.applicationId)
+  
+}
+var start = System.currentTimeMillis()
+f
+var end = System.currentTimeMillis() - start
+    
+   println("Shred,Standard,Query1,"+sf+","+Config.datapath+","+end+",total,"+spark.sparkContext.applicationId)
+ }
+}
