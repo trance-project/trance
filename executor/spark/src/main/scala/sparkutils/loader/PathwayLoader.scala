d12673eb0de894b71a17600a6ba85b916e362947
yaooo <yaoshi616@gmail.com>
2020-06-15 03:06:55 +0100

    add pathway loader

--- executor/spark/src/main/scala/sparkutils/loader/PathwayLoader.scala
+++ executor/spark/src/main/scala/sparkutils/loader/PathwayLoader.scala
@@ -3,9 +3,16 @@ import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql._
 import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.functions._
 
 case class Name(name: String)
 case class Pathway(name: String, url: String, gene_set: List[Name])
+
+case class IPathway(index:Long, name: String, url: String, gene_set: List[Name])
+case class SPathway(index:Long, name: String, url: String)
+
+case class SName(_1: Long, name: String)
+
 case class FlatPathway(name: String, p_gene: String)
 case class VariantPathway(contig: String, start: Int, name: String, gene: String, p_gene: String)
 
@@ -22,5 +29,16 @@ class PathwayLoader(spark: SparkSession, path: String) extends Serializable {
             }
         ).toDF.as[Pathway]
         pathways
+    }
+
+    def shredDS = {
+        val input = loadDS.withColumn("index", monotonically_increasing_id()).as[IPathway]
+        val pathways = input.drop("gene_set").withColumnRenamed("index", "gene_set").as[SVariant]
+        val gene_set = input.flatMap{
+            p => p.gene_set.map{
+                gene_set => SName(p.index, gene_set.name)
+            }
+        }.as[SName]
+        (pathways, gene_set.repartition($"_1"))
     }
 }
