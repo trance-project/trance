1772080f4ab3140655af3095bda5bc8b65f6fa84
yaooo <yaoshi616@gmail.com>
2020-07-08 21:16:29 +0100

    cache and count inputs

--- compiler/src/main/scala/framework/examples/Genomic.scala
+++ compiler/src/main/scala/framework/examples/Genomic.scala
@@ -6,31 +6,55 @@ trait GenomicSchema extends Query {
 
   def loadTables(shred: Boolean = false, skew: Boolean = false): String = {
     if (shred)
-      s"""|val vloader = new VariantLoader(spark, "/mnt/app_hdd/data/Data/Variants/sub_chr22.vcf")
+      s"""|val vloader = new VariantLoader(spark, "/mnt/app_hdd/data/Data/Variants/ALL.chr22.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf")
           |val (variants, genotypes) = vloader.shredDS
           |val IBag_variants__D = variants
           |val IDict_variants__D_genotypes = genotypes
+          |IBag_variants__D.cache
+          |IBag_variants__D.count
+          |IDict_variants__D_genotypes.cache
+          |IDict_variants__D_genotypes.count
           |
-                |val cloader = new TGenomesLoader(spark)
+          |val cloader = new TGenomesLoader(spark)
           |val IBag_metadata__D = cloader.load("/mnt/app_hdd/data/Data/Phenotype/1000g.csv")
+          |IBag_metadata__D.cache
+          |IBag_metadata__D.count
+          |
           |val gtfLoader = new GTFLoader(spark, "/mnt/app_hdd/data/Data/Map/Homo_sapiens.GRCh37.87.chr.gtf")
           |val Gtfs = gtfLoader.loadDS
           |val IBag_Gtfs__D = Gtfs
+          |IBag_Gtfs__D.cache
+          |IBag_Gtfs__D.count
           |
-                |val pathwayLoader = new PathwayLoader(spark, "/mnt/app_hdd/data/Data/Pathway/c2.cp.v7.1.symbols.gmt")
+          |val pathwayLoader = new PathwayLoader(spark, "/mnt/app_hdd/data/Data/Pathway/c2.cp.v7.1.symbols.gmt")
           |val (pathways, geneSet) = pathwayLoader.shredDS
           |val IBag_pathways__D = pathways
           |val IDict_pathways__D_gene_set = geneSet
+          |IBag_pathways__D.cache
+          |IBag_pathways__D.count
+          |
           |""".stripMargin
     else
-      s"""|val vloader = new VariantLoader(spark, "/mnt/app_hdd/data/Data/Variants/sub_chr22.vcf")
+      s"""|val vloader = new VariantLoader(spark, "/mnt/app_hdd/data/Data/Variants/ALL.chr22.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf")
           |val variants = vloader.loadDS
+          |variants.cache
+          |variants.count
+          |
           |val cloader = new TGenomesLoader(spark)
           |val metadata = cloader.load("/mnt/app_hdd/data/Data/Phenotype/1000g.csv")
+          |metadata.cache
+          |metadata.count
+          |
           |val gtfLoader = new GTFLoader(spark, "/mnt/app_hdd/data/Data/Map/Homo_sapiens.GRCh37.87.chr.gtf")
           |val Gtfs = gtfLoader.loadDS
+          |Gtfs.cache
+          |Gtfs.count
+          |
           |val pathwayLoader = new PathwayLoader(spark, "/mnt/app_hdd/data/Data/Pathway/c2.cp.v7.1.symbols.gmt")
           |val pathways = pathwayLoader.loadDS
+          |pathways.cache
+          |pathways.count
+          |
           |""".stripMargin
   }
 
