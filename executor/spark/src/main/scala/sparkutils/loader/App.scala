92bdcbb4cb900144132ae308b65b09eafce77d8f
Jaclyn Smith <jacalacka@gmail.com>
2020-09-23 14:22:41 -0700

    cleaning up loader up

--- executor/spark/src/main/scala/sparkutils/loader/App.scala
+++ executor/spark/src/main/scala/sparkutils/loader/App.scala
@@ -15,72 +15,9 @@ object App {
      val spark = SparkSession.builder().config(conf).getOrCreate()
 
      import spark.implicits._
-     //val vloader = new VariantLoader(spark, "/nfs_qc4/genomics/sub.vcf")
-     //val variants = vloader.loadVCF
-     //variants.take(10).foreach(println(_))
-	 //val annots = veploader.loadAnnotations(variants)
-     //annots.cache
-     //annots.count 
-     //println("annotations here")
-     //println(annots.show())
-	 //annots.take(10).foreach(println(_))
-	 //val veploader = new VepLoader(spark)
-	 //val mloader = new MAFLoader(spark)
-	 //val fmaf = "TCGA.BRCA.mutect.995c0111-d90b-4140-bee7-3845436c3b42.DR-10.0.somatic.maf"
-	 //val fmaf = ""
-	 //val fmaf = "small.maf"
-	 //val maf = mloader.loadFlat(s"/nfs_qc4/genomics/gdc/somatic/mafs/TCGA.DLBC.mutect.c3df46a9-85d1-45d4-954a-825313d4a26d.DR-10.0.somatic.maf/", dir = false)
 
-	 //val occurrences = veploader.loadOccurrencesMid(maf)
-	 //occurrences.cache
-	 //occurrences.count
-	 //val (vind, annots) = veploader.loadOccurrencesMid(maf)
-	 //println(occurrences.printSchema)
-	 //occurrences.take(10).foreach(println(_))
-	 //occurrences.write.format("json").save("file:///nfs_qc4/genomics/gdc/somatic/datasetDLBC/")
-	 /**
-	 //one.write.format("json").save("file://nfs_qc4/genomics/gdc/somatic/vindexed/")
-	 //annotations.take(10).foreach(println(_))
-	 //val occurences = spark.read.json("file:///nfs_qc4/genomics/gdc/somatic/dataset/").as[VepAnnotTrunc]
-	 //occurrences.take(10).foreach(println(_))
-	 //annotations.take(10).foreach(println(_))
-	 val joined = veploader.buildOccurrences(occurrences, annotations)
-	 //joined.take(100).foreach(println(_))
-	 val total = joined.count
-	 println(s"writing out this many somatic mutations $total")
-	 joined.write.format("json").save("file:///nfs_qc4/genomics/gdc/somatic/dataset/")
-	 val occurrences = veploader.buildOccurrences(maf, "file:///nfs_qc4/genomics/gdc/somatic/dataset/")
-	 val stats = occurrences.map(o => o.transcript_consequences match {
-		case Some(ts) => (ts.size, ts.map(t => t.consequence_terms.size))
-		case None => (0, Nil)
-	 })
-	 stats.collect.foreach(println(_))**/
-   //       val gloader = new GisticLoader(spark)
-   //       val gistic = gloader.merge("/nfs_qc4/genomics/gdc/gistic/")
-   //       gistic.count
-		 // gistic.write.format("json").save("file:///nfs_qc4/genomics/gdc/gistic/dataset")
-	/**val bloader = new BiospecLoader(spark)
-	val biospec = bloader.load("/nfs_qc4/genomics/gdc/biospecimen/")
-	println(biospec.show())
-       val cloader = new ConsequenceLoader(spark)
-       val thresh = cloader.loadSequential("/nfs_qc4/genomics/calc_variant_conseq.txt")
-       thresh.collect.foreach(println(_))
-   val geloader = new GeneExpressionLoader(spark)
-   val gexpr = geloader.load("/nfs_qc4/genomics/gdc/fpkm_uq", dir = true)
-   println(gexpr.show())**/
-    //val bloader = new BiomartLoader(spark)
-    //val biomart = bloader.load("/home/jacith/shredder/executor/spark/data/geno/mart_export.txt")
-    //println(biomart.show())
-	//val cloader = new CopyNumberLoader(spark)
-	//val copynum = cloader.load("/nfs_qc4/genomics/gdc/gene_level/", true)
-	//copynum.take(10).foreach(println(_))
-
-	//val gtfLoader = new GTFLoader(spark, "/nfs_qc4/genomics/Homo_sapiens.GRCh37.87.chr.gtf")
-	//val gtf = gtfLoader.loadDS
-	//gtf.take(10).foreach(println(_))
-    val nl = new NetworkLoader(spark)
-    val n = nl.load("/Users/jac/bioqueries/data/ppi/9606.protein.links.full.v11.0.csv")
-    n.write.format("json").save("file:///Users/jac/bioqueries/data/ppi/etwork")
-    }
+     // TESTING AREA FOR LOADERS
+       
+  }
 
 }
