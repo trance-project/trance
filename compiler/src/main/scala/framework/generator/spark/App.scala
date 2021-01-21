97afb8b2d233cfbad1c82977b4e51cecc20fdc66
Jaclyn Smith <jacalacka@gmail.com>
2021-01-20 16:56:59 -0800

    separate occurrences load function

--- compiler/src/main/scala/framework/generator/spark/App.scala
+++ compiler/src/main/scala/framework/generator/spark/App.scala
@@ -11,22 +11,22 @@ object E2EApp extends App {
   override def main(args: Array[String]){
 
     // standard pipeline
-    // AppWriter.runDataset(HybridBySampleNew, "HybridBySampleNew,standard", optLevel = 1)
-    // AppWriter.runDataset(SampleNetworkNew, "SampleNetworkNew,standard", optLevel = 1)
-    // AppWriter.runDataset(EffectBySampleNew, "EffectBySampleNew,standard", optLevel = 1)
-    // AppWriter.runDataset(ConnectionBySampleNew, "ConnectionBySampleNew,standard", optLevel = 1)
-    //AppWriter.runDataset(GeneConnectivityNew, "GeneConnectivityNew,standard", optLevel = 2)
+    AppWriter.runDataset(HybridBySampleNew, "HybridBySampleNew,standard", optLevel = 1)
+    AppWriter.runDataset(SampleNetworkNew, "SampleNetworkNew,standard", optLevel = 1)
+    AppWriter.runDataset(EffectBySampleNew, "EffectBySampleNew,standard", optLevel = 1)
+    AppWriter.runDataset(ConnectionBySampleNew, "ConnectionBySampleNew,standard", optLevel = 1)
+    AppWriter.runDataset(GeneConnectivityNew, "GeneConnectivityNew,standard", optLevel = 1)
 
     // shredded pipeline
-    // AppWriter.runDatasetShred(HybridBySampleNewS, "HybridBySampleNew,shredded", optLevel = 1)
-    // AppWriter.runDatasetShred(SampleNetworkNew, "SampleNetworkNew,shredded", optLevel = 1)
-    // AppWriter.runDatasetShred(EffectBySampleNew, "EffectBySampleNew,shredded", optLevel = 1)
-    // AppWriter.runDatasetShred(ConnectionBySampleNew, "ConnectionBySampleNew,shredded", optLevel = 1)
-    //AppWriter.runDatasetShred(GeneConnectivityNew, "GeneConnectivityNew,shredded", optLevel = 2)
+    AppWriter.runDatasetShred(HybridBySampleNewS, "HybridBySampleNew,shredded", optLevel = 1)
+    AppWriter.runDatasetShred(SampleNetworkNew, "SampleNetworkNew,shredded", optLevel = 1)
+    AppWriter.runDatasetShred(EffectBySampleNew, "EffectBySampleNew,shredded", optLevel = 1)
+    AppWriter.runDatasetShred(ConnectionBySampleNew, "ConnectionBySampleNew,shredded", optLevel = 1)
+    AppWriter.runDatasetShred(GeneConnectivityNew, "GeneConnectivityNew,shredded", optLevel = 1)
 
-	  //AppWriter.runDataset(BuildOccurrences2, "Build,standard", optLevel = 1)
-	  //AppWriter.runDatasetShred(BuildOccurrences1, "Build,shredded", optLevel = 1)
-    //AppWriter.runDataset(HybridBySampleMuts, "HybridScore,standard", optLevel = 1)
+	  // AppWriter.runDataset(BuildOccurrences2, "Build,standard", optLevel = 1)
+	  // AppWriter.runDatasetShred(BuildOccurrences1, "Build,shredded", optLevel = 1)
+    // AppWriter.runDataset(HybridBySampleMuts, "HybridScore,standard", optLevel = 1)
     AppWriter.runDatasetShred(HybridBySampleMuts, "HybridScore,shredded", optLevel = 1)
 
   }
