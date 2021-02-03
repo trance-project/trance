226cc0515c8da56c721864d6f5084ac835aaccef
Jaclyn Smith <jacalacka@gmail.com>
2021-02-03 09:03:14 -0800

    remove extra impact

--- compiler/src/main/scala/framework/examples/genomic/Annotations.scala
+++ compiler/src/main/scala/framework/examples/genomic/Annotations.scala
@@ -157,7 +157,6 @@ trait Vep {
     "flags" -> BagType(element),
     "gene_id" -> StringType,
     "impact" -> StringType,
-    "impact2" -> DoubleType,
     "intron" -> StringType,
     "polyphen_prediction" -> StringType,
     "polyphen_score" -> DoubleType,
