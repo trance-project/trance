f77c1c749914958a503ee4cdd0d48d8d64b06b75
Jaclyn Smith <jacmarjorie@gmail.com>
2021-04-06 18:29:46 -0700

    json print an nrc query

--- compiler/src/main/scala/framework/nrc/JsonWriter.scala
+++ compiler/src/main/scala/framework/nrc/JsonWriter.scala
@@ -1,9 +1,8 @@
 package framework.nrc
 
 import scala.collection.mutable._
-import net.liftweb.json._
-import net.liftweb.json.Serialization.writePretty
 import java.io._
+import play.api.libs.json.Json
 
 import framework.common._
 import framework.examples.tpch._
@@ -12,13 +11,128 @@ import framework.nrc._
 import scala.collection.immutable.Map
 // import framework.plans.{Equals => CEquals, Project => CProject}
 
-object JsonWriter extends MaterializeNRC {
+trait JsonBasedPrinter extends Printer {
+	this: MaterializeNRC =>
 
-	// very simple way 
-	def getJsonString(query: Program): String = {
-		implicit val formats = DefaultFormats
-		writePretty(query)
-	}
+  def ind(in: String, n: Int = 0): String = s" $in "
+ 
+  override def quote(e: Expr): String = e match {
+    // case c: Const if c.tp == StringType => c.v
+    //   // "\"" + c.v + "\""
+    case c: Const =>
+      c.v.toString
+	case Udf(n, e1, tp) => s"$n(${quote(e1)})"
+	case v: VarRef =>
+      v.name
+    case p: Project =>
+      quote(p.tuple) + "." + p.field
+    case ForeachUnion(x, e1, e2) =>
+      s"""|For ${x.name} in ${quote(e1)} Union
+          |${ind(quote(e2))}""".stripMargin
+    case Union(e1, e2) =>
+      s"""|(${quote(e1)})
+          |Union
+          |(${quote(e2)})""".stripMargin
+    case Singleton(e1) => s"""", "labels": [${quote(e1)}] """
+    case DeDup(e1) => s"DeDup(${quote(e1)})"
+    case Get(e1) => s"Get(${quote(e1)})"
+    case Tuple(fs) => 
+    	val lbls = fs.map(f => f._2.tp match {
+    		case _:BagType => s"""{ "name": "${f._1}", "key": "${quote(f._2)} }"""
+    		case _ => s"""{ "name": "${f._1}", "key": "${quote(f._2)}" }"""
+    	})
+    	s"""${lbls.mkString(",\n")}"""
+      //s"( ${fs.map { case (k, v) => k + " := " + quote(v) }.mkString(",\n   ")} )"
+    case l: Let =>
+      s"""|Let ${l.x.name} =
+          |${ind(quote(l.e1))} 
+          |In ${quote(l.e2)}""".stripMargin
+    case c: Cmp =>
+      s"${quote(c.e1)} ${c.op} ${quote(c.e2)}"
+    case And(e1, e2) =>
+      s"${quote(e1)} AND ${quote(e2)}"
+    case Or(e1, e2) =>
+      s"${quote(e1)} OR ${quote(e2)}"
+    case Not(e1) =>
+      s"NOT ${quote(e1)}"
+    case i: IfThenElse =>
+      if (i.e2.isDefined)
+        s"""|If (${quote(i.cond)}) Then
+            |${ind(quote(i.e1))}
+            |Else
+            |${ind(quote(i.e2.get))}""".stripMargin
+      else
+        s"""|If (${quote(i.cond)}) Then
+            |${ind(quote(i.e1))}""".stripMargin
+    case ArithmeticExpr(op, e1, e2) =>
+      s"(${quote(e1)} $op ${quote(e2)})"
+    case Count(e1) => s"Count(${quote(e1)})"
+    case Sum(e1, fs) =>
+      s"Sum(${quote(e1)}, (${fs.mkString(", ")}))"
+    case GroupByKey(e, ks, vs, _) =>
+      s"""|GroupByKey([${ks.mkString(", ")}], [${vs.mkString(", ")}],
+          |${ind(quote(e))}
+          |)""".stripMargin
+    case ReduceByKey(e, ks, vs) =>
+      s"""|ReduceByKey[${ks.mkString(", ")}], [${vs.mkString(", ")}],
+          |${ind(quote(e))}
+          |""".stripMargin
+
+    // Label extensions
+    case x: ExtractLabel =>
+//      val tuple = x.lbl.tp.attrTps.keys.mkString(", ")
+      val tuple = x.lbl.tp.attrTps.map(x => x._1 + " : " + quote(x._2)).mkString(", ")
+      s"""|Extract ${quote(x.lbl)} as ($tuple) In
+          |${quote(x.e)}""".stripMargin
+    case l: NewLabel =>
+      val ps = l.params.map { case (n, p) => n + " := " + quote(p.e) }.toList
+//      val ps = l.params.map { case (n, p) => n + " := " + quote(p.e) + " : " + quote(p.tp)}.toList
+      s"NewLabel(${(l.id :: ps).mkString(", ")})"
+
+    // Dictionary extensions
+    case EmptyDict =>
+      "Nil"
+    case BagDict(tp, flat, dict) =>
+      val params = tp.attrTps.keys.mkString(", ")
+//      val params = tp.attrTps.map(x => x._1 + ": " + quote(x._2)).mkString(", ")
+      s"""|(($params) ->
+          |  flat :=
+          |${ind(quote(flat), 2)},
+          |  tupleDict :=
+          |${ind(quote(dict), 2)}
+          |)""".stripMargin
+    case TupleDict(fs) =>
+      s"( ${fs.map { case (k, v) => k + " := " + quote(v) }.mkString(",\n   ")} )"
+    case TupleDictProject(v) => quote(v) + ".tupleDict"
+    case d: DictUnion =>
+      s"""|(${quote(d.dict1)})
+          |DictUnion
+          |(${quote(d.dict2)})""".stripMargin
+
+    // Shredding extensions
+    case ShredUnion(e1, e2) =>
+      s"""|(${quote(e1)})
+          |ShredUnion
+          |(${quote(e2)})""".stripMargin
+    case Lookup(lbl, dict) =>
+      s"Lookup(${quote(lbl)}, ${quote(dict)})"
+
+    // Materialization extensions
+    case MatDictLookup(lbl, dict) =>
+      s"MatDictLookup(${quote(lbl)}, ${quote(dict)})"
+    case BagToMatDict(b) =>
+      s"BagToMatDict(${quote(b)})"
+    case MatDictToBag(d) =>
+      s"MatDictToBag(${quote(d)})"
+
+    case _ =>
+      sys.error("Cannot print unknown expression " + e)
+  }
+
+
+}
+
+object JsonWriter extends MaterializeNRC with JsonBasedPrinter {
 
 	// obviously support for more types here...
 	def produceJsonString(tp: Type): String = tp match {
@@ -29,29 +143,21 @@ object JsonWriter extends MaterializeNRC {
 		case _ => s""" "${tp.toString()}" """
 	}
 
-	def produceJsonString(query: Assignment): String = 
-		s"""{"name": ${query.name}, "rhs": ${produceJsonString(query.rhs)}}"""
+	def produceJsonString(query: Assignment): String = {
+		val exp = query.rhs.tp match {
+			case _:BagType => s""" "${quote(query.rhs)} """
+			case _ => s""" "${quote(query.rhs)}" """
+		}
+		s"""{"name": "${query.name}", "key": $exp """
+	}
 
 	def produceJsonString(query: Program): String = 
-		s"""[{"assignments": ${query.statements.map(e => produceJsonString(e)).mkString(",")} }]"""
+		s"""[${query.statements.map(e => produceJsonString(e)).mkString(",")}}]"""
 
 	def produceJsonString(query: VarDef): String = 
 		s"""{ "name": ${query.name}, "type": ${produceJsonString(query.tp)} }"""
 
-	def produceJsonString(query: Expr): String = query match {
-		case f:ForeachUnion => 
-			s"""
-			|{
-			|	"exp": "for",
-			|	"attributes": {
-			|		"var": "${produceJsonString(f.x)}",
-			|		"in": "${produceJsonString(f.e1)}",
-			|	},
-			|	"union": [${produceJsonString(f.e2)}]
-			|}
-			""".stripMargin
-		case _ => s"""{"todo": "TODO"}"""
-	}
+	def produceJsonString(query: Expr): String = quote(query)
 
 }
 
@@ -109,14 +215,14 @@ object JsonWriterTest extends App with MaterializeNRC {
 			                          else if (t.impact = "LOW") then 0.30
 			                          else 0.01 )}).sumBy({gene}, {score}) )} )}
       """
-    val query1 = parser.parse(queryComplicate).get.asInstanceOf[JsonWriter.Program]
+    val query1 = parser.parse(querySimple).get.asInstanceOf[JsonWriter.Program]
 
-	val jsonRep = JsonWriter.getJsonString(query1)
+    val s = JsonWriter.produceJsonString(query1).replace("\n", "")
+    val jsValue = Json parse s
+    val pj = Json prettyPrint jsValue
 
 	val printer = new PrintWriter(new FileOutputStream(new File("test.json"), false))
-    printer.println(jsonRep)
+    printer.println(pj)
     printer.close
-
-    println(JsonWriter.produceJsonString(query1))
 
 }
