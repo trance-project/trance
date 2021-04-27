ee4c423e0f12e8c2c7128140c75e936162dc78ef
BrandonBoyWonder <brandon.bob.moore@gmail.com>
2021-04-27 20:03:35 +0100

    index on trance_webapp: 30b44710 playing with types

---
+++ service/app/models/TranceObject.scala
@@ -0,0 +1,11 @@
+package models
+
+import java.util.UUID
+
+/**
+  * Model for the Object to be sent to trance webapp to define databases object information
+  */
+case class TableObjects(
+                       _id: Option[UUID],
+                       
+                       )
