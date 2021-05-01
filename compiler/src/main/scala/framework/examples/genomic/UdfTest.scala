0867e0b3c1bc70935c804b1e94757b67d73726b2
PavlosPiperis <58398272+PavlosPiperis@users.noreply.github.com>
2021-05-01 17:47:41 +0100

    01/05

--- compiler/src/main/scala/framework/examples/genomic/UdfTest.scala
+++ compiler/src/main/scala/framework/examples/genomic/UdfTest.scala
@@ -1,8 +1,7 @@
 package framework.examples.genomic
 
-import framework.common._
-import framework.examples.Query
-import framework.nrc.Parser
+import java.util.Base64
+// test
 
 // this is just the example from the 
 object ExampleQuery extends DriverGene {
@@ -21,38 +20,38 @@ object ExampleQuery extends DriverGene {
           |val expression = geLoader.load("/mnt/app_hdd/data/expression", true)
           |
           |val occurrences = spark.read.json("/mnt/app_hdd/data/somatic/datasetPRAD")
+          |val pathways =
           |""".stripMargin
     }
   
   // name to identify your query
   val name = "ExampleQuery"
-  
+  val genetype = TupleType("name" -> StringType)
+  val pathtype = TupleType("p_name" -> StringType, "url" -> StringType, "gene_set" -> BagType(genetype))
+
   // a map of input types for the parser
     val tbls = Map("occurrences" -> occurmids.tp,
                     "copynumber" -> copynum.tp,
-                    "samples" -> samples.tp)
+                    "samples" -> samples.tp
+                    "pathways" -> BagType(pathtype))
+
 
   // a query string that is passed to the parser
   // note that a list of assignments should be separated with ";"
   val query = 
     s"""
-      cnvCases1 <= 
-        for s in samples union 
-          for c in copynumber union 
-            if (s.bcr_aliquot_uuid = c.cn_aliquot_uuid)
-            then {(sid := s.bcr_patient_uuid, gene := c.cn_gene_id, cnum := c.cn_copy_number)};
+        GMB <=
+          for p in pathway union
+            {(pathway := p.p_name, burdens :=
+              (for o in occurences union
+                for t in o.candidates union
+                  for g in p.gene_set union
+                    if (g.name = t.gene) then
+                      {(sid := s.sid, burden := if (t.impact = "HIGH") then 0.80
+                                                else if (t.impact = "MODERATE") then 0.50
+                                                else if (t.impact = "LOW") then 0.30
+                                                else 0.01)}).sumBy({sid}, {score}))}
 
-      hybridScore1 <=
-          for o in occurrences union
-            {( oid := o.oid, sid1 := o.donorId, cands1 :=
-              ( for t in o.transcript_consequences union
-                 if (t.sift_score > 0.0)
-                 then for c in cnvCases1 union
-                    if (t.gene_id = c.gene && o.donorId = c.sid) then
-                      {( gene1 := t.gene_id, score1 := (c.cnum + 0.01) * if (t.impact = "HIGH") then 0.80
-                          else if (t.impact = "MODERATE") then 0.50
-                          else if (t.impact = "LOW") then 0.30
-                          else 0.01 )}).sumBy({gene1}, {score1}) )}
     """
 
     // finally define the parser, note that it takes the input types 
--- compiler/src/main/scala/framework/generator/spark/App.scala
+++ compiler/src/main/scala/framework/generator/spark/App.scala
@@ -8,35 +8,54 @@ import framework.examples.genomic._
 import framework.examples.Query
 import framework.loader.csv._
 
+
+
+object TestZeppelin extends App {
+  override def main(args: Array[String]){
+    AppWriter.runDataset(ExampleQuery, "ExampleQuery,standard", optLevel = 1,
+      zhost = "qc-3", zport = 8085, notebk = true)
+  }
+}
+
+object LetExplore extends App {
+  override def main(args: Array[String]){
+    val dlbctest = 5000
+    val lenv = new LetTestEnv(dlbctest, shred=true)
+    val ests = lenv.estimates
+    AppWriter.runDatasetShred(lenv.queries.head, "Inlined,shredded", optLevel = 1)
+    // AppWriter.runDatasetShred(lenv.queries.last, "Sequential,shredded", optLevel = 1)
+  }
+}
+
 object Sharing extends App {
 
   override def main(args: Array[String]){
-  // another thing to test
-  // val dlbctest = 371520
-  // total is ~4500 so way underestimated
-	val dlbctest = 5000
-	// upper bound
-	val brcatest = 8000000
+
+    // val dlbctest = 371520
+    // total is ~4500 so way underestimated
+    // upper bound
+    val dlbctest = 5000
+    val brcatest = 8000000
     // capacity in KB
     val flex = 0
     // val ptype = "dynamic"
     val ptype = "greedy"
-    val genv = new GenomicEnv(dlbctest, init_flex = flex, ptype = ptype)
-    val sgenv = new GenomicEnv(dlbctest, init_shred = true, init_flex = flex, ptype = ptype)
-    AppWriter.runWithCache(genv, "CacheTest,standard,covers")
+    // val genv = new GenomicEnv(dlbctest, init_flex = flex, ptype = ptype)
+    val sgenv = new GenomicEnv(dlbctest, shred = true, flex = flex, ptype = ptype)
+    // AppWriter.runWithCache(genv, "CacheTest,standard,covers")
     AppWriter.runWithCache(sgenv, "CacheTest,shredded,covers")
 
     // cache inputs
-    AppWriter.runWithCache(genv, "CacheTest,standard,cacheinputs", cache = true)
-    AppWriter.runWithCache(sgenv, "CacheTest,shredded,cacheinputs", cache = true)
+    // AppWriter.runWithCache(genv, "CacheTest,standard,cacheinputs", cache = true)
+    // AppWriter.runWithCache(sgenv, "CacheTest,shredded,cacheinputs", cache = true)
 
-  // AppWriter.runDataset(HybridQuery, "Testing,standard", optLevel=1)
+    // AppWriter.runDataset(HybridQuery, "Testing,standard", optLevel=1)
 
-	// AppWriter.runDatasetShred(SequentialFilters, "SequentialFilters,shredded", optLevel = 1)
-  // AppWriter.runDatasetShred(SharedFilters, "SharedFilters,shredded", optLevel = 1)
-  // AppWriter.runDataset(OccurGroupByCase, "OccurGroupByCase,standard", optLevel = 1)
-  // AppWriter.runDataset(SamplesFilterByTP53, "SamplesFilterByTP53,standard", optLevel = 1)
-  // AppWriter.runDatasetShred(SamplesFilterByTP53, "SamplesFilterByTP53,standard", optLevel = 1)
+    // AppWriter.runDatasetShred(SequentialFilters, "SequentialFilters,shredded", optLevel = 1)
+    // AppWriter.runDatasetShred(SharedFilters, "SharedFilters,shredded", optLevel = 1)
+    // AppWriter.runDataset(OccurGroupByCase, "OccurGroupByCase,standard", optLevel = 1)
+    // AppWriter.runDataset(SamplesFilterByTP53, "SamplesFilterByTP53,standard", optLevel = 1)
+    // AppWriter.runDatasetShred(SamplesFilterByTP53, "SamplesFilterByTP53,standard", optLevel = 1)
 
   }
 
@@ -66,8 +85,8 @@ object E2EApp extends App {
     //AppWriter.runDatasetShred(GeneConnectivityNew, "GeneConnectivityNew,shredded", optLevel = 1)
 
     // AppWriter.runDatasetShred(ClinicalRunExample, "ClinicalRunExample,shredded", optLevel=1)
-	  // AppWriter.runDataset(BuildOccurrences2, "Build,standard", optLevel = 1)
-	  // AppWriter.runDatasetShred(BuildOccurrences1, "Build,shredded", optLevel = 1)
+    // AppWriter.runDataset(BuildOccurrences2, "Build,standard", optLevel = 1)
+    // AppWriter.runDatasetShred(BuildOccurrences1, "Build,shredded", optLevel = 1)
     // AppWriter.runDataset(HybridBySampleMuts, "HybridScore,standard", optLevel = 1)
     //AppWriter.runDatasetShred(HybridBySampleMuts, "HybridScore,shredded", optLevel = 1)
     //AppWriter.runDataset(ClinicalRunExample, "Likelihood,standard", optLevel = 1)
@@ -85,7 +104,7 @@ object App {
 
   val pathout = "../executor/spark/src/main/scala/sparkutils/generated/"
   val schema = TPCHSchema.getSchema()
- 
+
   def main(args: Array[String]){
     // runFlatToNested()
     runNestedToNested()
@@ -94,21 +113,21 @@ object App {
   }
 
   def runFlatToNested(){
-    
+
     // standard pipeline - all optimizations
     AppWriter.runDataset(Test2Flat, "Flat,2", schema = schema)
-    
+
     // shredded pipeline + unshredding
     AppWriter.runDatasetShred(Test2, "Shred,2", unshred=false, schema = schema)
 
   }
- 
+
   def runNestedToNested(){
-    
+
     // standard pipeline - all optimizations
     // AppWriter.runDatasetInput(Test0Full, Test0NN, "Flat,0")
     AppWriter.runDatasetInput(Test2FullFlat, Test2NN, "Flat,2", schema = schema)
-    
+
     // shredded pipeline + unshredding
     // AppWriter.runDatasetInputShred(Test2Full, Test2NN, "Shred,2", unshred=false, schema = schema)
   }
@@ -121,21 +140,21 @@ object App {
 
     // shredded pipeline + unshredding
     AppWriter.runDatasetInputShred(Test2Full, Test2Agg2S, "Shred,Standard,2", optLevel = 1, schema = schema)
-  
+
   }
 
   def runSkewHandling(){
 
-    // standard pipeline - all optimizations 
+    // standard pipeline - all optimizations
     // AppWriter.runDatasetInput(Test2Flat, Test2NNL, "Flat,Standard,2")
-    // standard pipeline - skew-handling - all optimizations 
+    // standard pipeline - skew-handling - all optimizations
     AppWriter.runDatasetInput(Test2Flat, Test2NNL, "Flat,Skew,2", skew = true, schema = schema)
 
     // shredded pipeline + unshredding
     // AppWriter.runDatasetInputShred(Test2, Test2NNL, "Shred,Standard,2", unshred=true)
-    // shredded pipeline + unshredding - skew-handling 
+    // shredded pipeline + unshredding - skew-handling
     AppWriter.runDatasetInputShred(Test2, Test2NNL, "Shred,Skew,2", unshred = true, skew = true, schema = schema)
-  
+
   }
 
 
