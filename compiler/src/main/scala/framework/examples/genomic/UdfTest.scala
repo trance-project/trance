9b4b086a0d47a563064495c1bff5d8c9e35ce775
PavlosPiperis <58398272+PavlosPiperis@users.noreply.github.com>
2021-05-02 20:51:21 +0100

    m

--- compiler/src/main/scala/framework/examples/genomic/UdfTest.scala
+++ compiler/src/main/scala/framework/examples/genomic/UdfTest.scala
@@ -48,11 +48,10 @@ object ExampleQuery extends DriverGene {
     s"""
         mapPathways <=
           for p in pathways union
-            {(pathway := p.p_name, gene_set :=
               for g in p.gene_set union
                 for g2 in genemap union
                   if (g.name = g2.g_gene_name) then
-                     {(name := g2.g_gene_id)})};
+                     {(pathway := p.p_name, name := g2.g_gene_id)})};
 
         GMB <=
           for p in mapPathways union
