074da498eac2b315823182a5f49725d7c37dd369
PavlosPiperis <58398272+PavlosPiperis@users.noreply.github.com>
2021-05-02 19:38:49 +0100

    notebook input

--- compiler/src/main/scala/framework/examples/genomic/UdfTest.scala
+++ compiler/src/main/scala/framework/examples/genomic/UdfTest.scala
@@ -59,9 +59,8 @@ object ExampleQuery extends DriverGene {
             {(pathway := p.pathway, burdens :=
               (for o in occurrences union
                   for g in p.gene_set union
-                    for g2 in genemap union
                       for t in o.transcript_consequences union
-                        if (g.name = g2.g_gene_name && g2.g_gene_id = t.gene_id) then
+                        if (g.name = t.gene_id) then
                            {(sid := o.donorId, burden := if (t.impact = "HIGH") then 0.80
                                                       else if (t.impact = "MODERATE") then 0.50
                                                       else if (t.impact = "LOW") then 0.30
