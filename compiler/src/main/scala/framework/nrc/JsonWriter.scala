5d135e79891c96ff98156491ffd7cddca6881142
Jaclyn Smith <jacmarjorie@gmail.com>
2021-05-05 16:03:16 -0700

    looking at shredding

--- compiler/src/main/scala/framework/nrc/JsonWriter.scala
+++ compiler/src/main/scala/framework/nrc/JsonWriter.scala
@@ -12,7 +12,7 @@ import scala.collection.immutable.Map
 // import framework.plans.{Equals => CEquals, Project => CProject}
 
 trait JsonBasedPrinter extends Printer {
-	this: MaterializeNRC =>
+  this: MaterializeNRC =>
 
   def ind(in: String, n: Int = 0): String = s" $in "
  
@@ -21,8 +21,8 @@ trait JsonBasedPrinter extends Printer {
     //   // "\"" + c.v + "\""
     case c: Const =>
       c.v.toString
-	case Udf(n, e1, tp) => s"$n(${quote(e1)})"
-	case v: VarRef =>
+  case Udf(n, e1, tp) => s"$n(${quote(e1)})"
+  case v: VarRef =>
       v.name
     case p: Project =>
       quote(p.tuple) + "." + p.field
@@ -37,11 +37,11 @@ trait JsonBasedPrinter extends Printer {
     case DeDup(e1) => s"DeDup(${quote(e1)})"
     case Get(e1) => s"Get(${quote(e1)})"
     case Tuple(fs) => 
-    	val lbls = fs.map(f => f._2.tp match {
-    		case _:BagType => s"""{ "name": "${f._1}", "key": "${quote(f._2)} }"""
-    		case _ => s"""{ "name": "${f._1}", "key": "${quote(f._2)}" }"""
-    	})
-    	s"""${lbls.mkString(",\n")}"""
+      val lbls = fs.map(f => f._2.tp match {
+        case _:BagType => s"""{ "name": "${f._1}", "key": "${quote(f._2)} }"""
+        case _ => s"""{ "name": "${f._1}", "key": "${quote(f._2)}" }"""
+      })
+      s"""${lbls.mkString(",\n")}"""
       //s"( ${fs.map { case (k, v) => k + " := " + quote(v) }.mkString(",\n   ")} )"
     case l: Let =>
       s"""|Let ${l.x.name} =
@@ -134,49 +134,69 @@ trait JsonBasedPrinter extends Printer {
 
 object JsonWriter extends MaterializeNRC with JsonBasedPrinter {
 
-	// obviously support for more types here...
-	def produceJsonString(tp: Type): String = tp match {
-		case BagType(t) => s"[${produceJsonString(t)}]"
-		case TupleType(fs) => 
-			val conts = fs.map(f => s""""${f._1}": ${produceJsonString(f._2)} """).mkString(",")
-			s"""{$conts}"""
-		case _ => s""" "${tp.toString()}" """
-	}
+  // obviously support for more types here...
+  def produceJsonString(tp: Type): String = tp match {
+    case BagType(t) => s"[${produceJsonString(t)}]"
+    case TupleType(fs) => 
+      val conts = fs.map(f => s""""${f._1}": ${produceJsonString(f._2)} """).mkString(",")
+      s"""{$conts}"""
+    case _ => s""" "${tp.toString()}" """
+  }
 
-	def produceJsonString(query: Assignment): String = {
-		val exp = query.rhs.tp match {
-			case _:BagType => s""" "${quote(query.rhs)} """
-			case _ => s""" "${quote(query.rhs)}" """
-		}
-		s"""{"name": "${query.name}", "key": $exp """
-	}
+  def produceJsonString(query: Assignment): String = {
+    val exp = query.rhs.tp match {
+      case _:BagType => s""" "${quote(query.rhs)} """
+      case _ => s""" "${quote(query.rhs)}" """
+    }
+    s"""{"name": "${query.name}", "key": $exp """
+  }
 
-	def produceJsonString(query: Program): String = 
-		s"""[${query.statements.map(e => produceJsonString(e)).mkString(",")}}]"""
+  def produceJsonString(query: Program): String = 
+    s"""[${query.statements.map(e => produceJsonString(e)).mkString(",")}}]"""
 
-	def produceJsonString(query: VarDef): String = 
-		s"""{ "name": ${query.name}, "type": ${produceJsonString(query.tp)} }"""
+  def produceJsonString(query: VarDef): String = 
+    s"""{ "name": ${query.name}, "type": ${produceJsonString(query.tp)} }"""
 
-	def produceJsonString(query: Expr): String = quote(query)
+  def produceJsonString(query: Expr): String = quote(query)
 
 }
 
-object JsonWriterTest extends App with MaterializeNRC {
+object JsonWriterTest extends App with Printer with Materialization with MaterializeNRC with Shredding {
 
-	val occur = new Occurrence{}
-	val cnum = new CopyNumber{}
-	val samps = new Biospecimen{}
-	val tbls = Map("Customer" -> TPCHSchema.customertype,
-	             "Order" -> TPCHSchema.orderstype,
-	             "Lineitem" -> TPCHSchema.lineittype,
-	             "Part" -> TPCHSchema.parttype, 
-	             "occurrences" -> BagType(occur.occurmid_type), 
-	             "copynumber" -> BagType(cnum.copyNumberType), 
-	             "samples" -> BagType(samps.biospecType))
+  val occur = new Occurrence{}
+  val cnum = new CopyNumber{}
+  val samps = new Biospecimen{}
+  val tbls = Map("Customer" -> TPCHSchema.customertype,
+               "Order" -> TPCHSchema.orderstype,
+               "Lineitem" -> TPCHSchema.lineittype,
+               "Part" -> TPCHSchema.parttype, 
+               "occurrences" -> BagType(occur.occurmid_type), 
+               "copynumber" -> BagType(cnum.copyNumberType), 
+               "samples" -> BagType(samps.biospecType))
 
-	val parser = Parser(tbls)
+  val parser = Parser(tbls)
 
-	val queryComplicate = 
+  def parseProgram(query: String, shred: Boolean = false): String = {
+
+    // parse the input query string
+    val program = parser.parse(query).get.asInstanceOf[JsonWriterTest.Program]
+
+    // shred if necessary
+    val compiled = if (shred){
+      val (shredded, shreddedCtx) = shredCtx(program)
+      val optShredded = optimize(shredded)
+      val materializedProgram = materialize(optShredded, eliminateDomains = true)
+      materializedProgram.program
+    }else program
+
+    println(quote(compiled))
+    
+    // use the json writer from framework.nrc to write 'er
+    JsonWriter.produceJsonString(compiled.asInstanceOf[JsonWriter.Program])
+
+  }
+
+  val queryComplicate = 
       s"""
         cnvCases1 <= 
           for c in copynumber union
@@ -196,33 +216,47 @@ object JsonWriterTest extends App with MaterializeNRC {
                           else 0.01 )}).sumBy({gene}, {score}) )}
       """
 
-    // this should be 			                    
+    // this should be                           
     // if (t.gene_id = c.cn_gene_id && c.cn_aliquot_uuid = s.bcr_aliquot_uuid) then
     // but i need to fix the parser 
     val querySimple = 
-    	s"""
-    	  QuerySimple <=
-    	  for s in samples union 
-   			{(sample := s.bcr_patient_uuid, mutations := 
-           		for o in occurrences union
-           			if (s.bcr_patient_uuid = o.donorId) then
-            		  {( mutId := o.oid, scores := 
-              			( for t in o.transcript_consequences union
-			                  for c in copynumber union
-			                    if (t.gene_id = c.cn_gene_id && c.cn_aliquot_uuid = s.bcr_aliquot_uuid) then
-			                      {( gene := t.gene_id, score := (c.cn_copy_number + 0.01) * if (t.impact = "HIGH") then 0.80 
-			                          else if (t.impact = "MODERATE") then 0.50
-			                          else if (t.impact = "LOW") then 0.30
-			                          else 0.01 )}).sumBy({gene}, {score}) )} )}
+      s"""
+        QuerySimple <=
+        for s in samples union 
+        {(sample := s.bcr_patient_uuid, mutations := 
+              for o in occurrences union
+                if (s.bcr_patient_uuid = o.donorId) then
+                  {( mutId := o.oid, scores := 
+                    ( for t in o.transcript_consequences union
+                        for c in copynumber union
+                          if (t.gene_id = c.cn_gene_id && c.cn_aliquot_uuid = s.bcr_aliquot_uuid) then
+                            {( gene := t.gene_id, score := (c.cn_copy_number + 0.01) * if (t.impact = "HIGH") then 0.80 
+                                else if (t.impact = "MODERATE") then 0.50
+                                else if (t.impact = "LOW") then 0.30
+                                else 0.01 )}).sumBy({gene}, {score}) )} )}
       """
-    val query1 = parser.parse(querySimple).get.asInstanceOf[JsonWriter.Program]
+    // val query1 = parser.parse(querySimple).get.asInstanceOf[JsonWriter.Program]
 
-    val s = JsonWriter.produceJsonString(query1).replace("\n", "")
+    // val s = JsonWriter.produceJsonString(query1).replace("\n", "")
+    // val jsValue = Json parse s
+    // val pj = Json prettyPrint jsValue
+
+    val soSimple = 
+      s"""
+        ShredTest <=
+          for o in occurrences union 
+            {(sid := o.donorId, cons := 
+              for t in o.transcript_consequences union 
+                {(gene := t.gene_id)}
+            )}
+      """
+
+    val s = parseProgram(soSimple, shred = true).replace("\n", "")
     val jsValue = Json parse s
     val pj = Json prettyPrint jsValue
 
-	val printer = new PrintWriter(new FileOutputStream(new File("test.json"), false))
-    printer.println(pj)
-    printer.close
+  // val printer = new PrintWriter(new FileOutputStream(new File("test.json"), false))
+ //    printer.println(pj)
+ //    printer.close
 
 }
