8547fc5f52964af5b39cd4d75921044d4691e876
Jaclyn Smith <jacmarjorie@gmail.com>
2021-05-12 15:34:50 -0700

    small skew test

--- compiler/src/main/scala/framework/examples/genomic/SkewExplore.scala
+++ compiler/src/main/scala/framework/examples/genomic/SkewExplore.scala
@@ -10,6 +10,66 @@ import framework.nrc.Parser
   *
   **/
 
+object SkewTestSmall0 extends DriverGene {
+
+  val name = "SkewTestSmall0"
+
+  override def loadTables(shred: Boolean = false, skew: Boolean = false): String =
+    if (shred){
+      s"""|val samples = spark.table("samples")
+          |val IBag_samples__D = samples
+          |
+          |val copynumber = spark.table("copynumber")
+          |val IBag_copynumber__D = copynumber
+          |
+          |val odict1 = spark.table("odict1")
+          |val IBag_occurrences__D = odict1
+          |
+          |// issue with partial shredding here
+          |val odict2 = spark.table("odict2").drop("flags")
+          |val IDict_occurrences__D_transcript_consequences = odict2
+          |
+          |val odict3 = spark.table("odict3")
+          |val IDict_occurrences__D_transcript_consequences_consequence_terms = odict3
+          |""".stripMargin
+    }else{
+      s"""|val samples = spark.table("samples")
+          |
+          |val copynumber = spark.table("copynumber")
+          |
+          |val occurrences = spark.table("occurrences")
+          |""".stripMargin
+    }
+
+  val name = "SkewTestSmall0"
+  
+  val tbls = Map("occurrences" -> occurmids.tp, 
+                  "copynumber" -> copynum.tp, 
+                  "samples" -> samples.tp)
+
+  val query = 
+    s"""
+      Centers <= 
+        dedup(
+          for s in samples union 
+            {(center := s.source_center)}
+        );
+
+      SkewTestSmall0 <= 
+      for c in Centers union 
+        {(center := c.center, mutations := 
+          for s in samples union 
+            if (s.source_center = c.center) then
+            for o in occurrences union 
+              if (s.bcr_patient_uuid = o.donorId) then 
+                {( oid := o.oid, sid := o.donorId )}
+        )}
+    """.stripMargin
+  val parser = Parser(tbls)
+  val program = parser.parse(query).get.asInstanceOf[Program]
+
+}
+
 object SkewTest0 extends DriverGene {
 
   val name = "SkewTest0"
