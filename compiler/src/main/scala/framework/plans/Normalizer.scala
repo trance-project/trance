0bfe23c98f05cf383fb1315c2166065620839bfb
Jaclyn Smith <jacmarjorie@gmail.com>
2021-05-16 09:36:12 -0700

    merge

--- compiler/src/main/scala/framework/plans/Normalizer.scala
+++ compiler/src/main/scala/framework/plans/Normalizer.scala
@@ -126,12 +126,6 @@ class BaseNormalizer(val letOpt: Boolean = false) extends BaseCompiler {
       case Comprehension(e2, v2, p2, e3) if (!letOpt) =>  
           Comprehension(e2, v2, p2, normalizeIf(comprehension(e3, p, e)))
 
-      // how to normalize in a nested??
-      case CReduceBy(in, v, ks, vs) if (!letOpt) => 
-        val nc = comprehension(in, (i: Rep) => Constant(true), (i: Rep) => i)
-        val v2 = Variable.freshFromBag(nc.tp)
-        CReduceBy(nc, v2, ks, vs)
-
       case _ => // standard case (return self)
         val v = Variable.freshFromBag(e1.tp)
         e(v) match {
