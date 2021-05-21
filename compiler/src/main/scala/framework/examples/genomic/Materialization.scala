5b8fc3392f0a5bf60c2325931cd0228af9e0e375
Jaclyn Smith <jacmarjorie@gmail.com>
2021-05-21 07:51:21 -0700

    materialization fail with a let

--- compiler/src/main/scala/framework/examples/genomic/Materialization.scala
+++ compiler/src/main/scala/framework/examples/genomic/Materialization.scala
@@ -516,10 +516,9 @@ object LetTest5 extends DriverGene {
       """
     val query = 
      s"""
-       initScores <= $initScores; 
-
        LetTest5 <= 
-        for i in initScores union
+       let initScores := $initScores 
+       in for i in initScores union
          for h in i.hybrid_scores union 
            {(hybrid_gene := h.hybrid_gene, hybrid_score := h.hybrid_score)}
      """
