952c9161a65fcd0040d875695741a04e0622f4b2
PavlosPiperis <58398272+PavlosPiperis@users.noreply.github.com>
2021-06-24 15:46:32 +0100

    Occurences and Gene Expression

--- compiler/src/main/scala/framework/examples/genomic/UdfTest.scala
+++ compiler/src/main/scala/framework/examples/genomic/UdfTest.scala
@@ -87,6 +87,8 @@ object ExampleQuery extends DriverGene {
     //     matrix <= matrix[selected_genes] // subset
     // """
 
+
+// Using Occurences (Impact score)
 //    s"""
 //        GMB <=
 //          for g in genemap union
@@ -111,28 +113,61 @@ object ExampleQuery extends DriverGene {
 //            )}
 //    """
 
-  // defined using fpkm only (Gene Expression)
-  s"""
+  // Using only Gene Expression (fpkm)
+//  s"""
+//        GMB <=
+//          for g in genemap union
+//            {(gene:= g.g_gene_name, fpkm :=
+//              (for e in expression union
+//                for c in clinical union
+//                  for s in samples union
+//                   if (s.bcr_patient_uuid = c.bcr_patient_uuid) then
+//                    if (e.ge_aliquot = s.bcr_aliquot_uuid) then
+//                      if (g.g_gene_id = e.ge_gene_id) then
+//                         {(sid := e.ge_aliquot,
+//                           lbl := if (c.gleason_pattern_primary = 2) then 0
+//                            else if (c.gleason_pattern_primary = 3) then 0
+//                            else if (c.gleason_pattern_primary = 4) then 1
+//                            else if (c.gleason_pattern_primary = 5) then 1
+//                            else -1,
+//                           fpkm := e.ge_fpkm
+//                          )}
+//              ).sumBy({sid, lbl}, {fpkm})
+//            )}
+//    """
+
+
+// Integrating occurences and gene expression
+
+    s"""
         GMB <=
           for g in genemap union
-            {(gene:= g.g_gene_name, fpkm :=
-              (for e in expression union
+            {(gene:= g.g_gene_name, burdens :=
+              (for o in occurrences union
                 for c in clinical union
-                  for s in samples union
-                   if (s.bcr_patient_uuid = c.bcr_patient_uuid) then
-                    if (e.ge_aliquot = s.bcr_aliquot_uuid) then
-                      if (g.g_gene_id = e.ge_gene_id) then
-                         {(sid := e.ge_aliquot,
-                           lbl := if (c.gleason_pattern_primary = 2) then 0
-                            else if (c.gleason_pattern_primary = 3) then 0
-                            else if (c.gleason_pattern_primary = 4) then 1
-                            else if (c.gleason_pattern_primary = 5) then 1
-                            else -1,
-                           fpkm := e.ge_fpkm
-                          )}
-              ).sumBy({sid, lbl}, {fpkm})
+                  for e in expression union
+                    for s in samples union
+                      if (s.bcr_patient_uuid = c.bcr_patient_uuid) then
+                        if (o.donorId = c.bcr_patient_uuid) then
+                          if (e.ge_aliquot = s.bcr_aliquot_uuid) then
+                            for t in o.transcript_consequences union
+                              if (g.g_gene_id = t.gene_id) then
+                                 {(sid := o.donorId,
+                                   lbl := if (c.gleason_pattern_primary = 2) then 0
+                                    else if (c.gleason_pattern_primary = 3) then 0
+                                    else if (c.gleason_pattern_primary = 4) then 1
+                                    else if (c.gleason_pattern_primary = 5) then 1
+                                    else -1,
+                                   burden := if (t.impact = "HIGH") then 0.80*e.fpkm
+                                                            else if (t.impact = "MODERATE") then 0.50*e.fpkm
+                                                            else if (t.impact = "LOW") then 0.30*e.fpkm
+                                                            else 0.01*e.fpkm
+                              )}
+              ).sumBy({sid, lbl}, {burden})
             )}
     """
+
+
   }
   // finally define the parser, note that it takes the input types
     // map as input and pass the query string to the parser to
