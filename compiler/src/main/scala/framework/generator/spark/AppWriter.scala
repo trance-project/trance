e069b3131acd6101278c9bc63d6737d2476c5eab
PavlosPiperis <58398272+PavlosPiperis@users.noreply.github.com>
2021-07-15 21:43:11 +0100

    adding paragraph

--- compiler/src/main/scala/framework/generator/spark/AppWriter.scala
+++ compiler/src/main/scala/framework/generator/spark/AppWriter.scala
@@ -98,7 +98,7 @@ object AppWriter {
       println(s"Writing out to $qname notebook with id: $noteid")
 
       // collect the contents of the files
-      var udftext : String
+      var udftext = Seq[String]()
       for (u <- codegen.udfsUsed) {
         val bufferedSource = scala.io.Source.fromFile(s"udfs/${u}.udf")
         udftext = udftext :+ bufferedSource.getLines().mkString
