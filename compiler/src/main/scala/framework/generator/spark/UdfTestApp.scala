5b03a55cd4471d3bb8068cbdfb2468fc6b037318
PavlosPiperis <58398272+PavlosPiperis@users.noreply.github.com>
2021-07-28 12:50:50 +0100

    udf test

--- compiler/src/main/scala/framework/generator/spark/UdfTestApp.scala
+++ compiler/src/main/scala/framework/generator/spark/UdfTestApp.scala
@@ -31,6 +31,8 @@ object UdfDevApp extends App {
   override def main(args: Array[String]){
 
     // runs the shredded pipeline to file
-    AppWriter.runDatasetShred(PivotUDFExample, "ExampleTest,standard", optLevel = 1)
+    AppWriter.runDatasetShred(PivotUDFExample, "ExampleTest,standard", optLevel = 1,notebk = true, zhost = "oda-compute-0-6",
+      //zhost = "localhost",
+      zport = 8085)
   }
 }
