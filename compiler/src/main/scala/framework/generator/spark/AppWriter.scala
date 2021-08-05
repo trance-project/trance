7e4c8cc039f09306f78d3e202e690e0628d6e333
PavlosPiperis <58398272+PavlosPiperis@users.noreply.github.com>
2021-08-05 19:48:52 +0300

    comma fix

--- compiler/src/main/scala/framework/generator/spark/AppWriter.scala
+++ compiler/src/main/scala/framework/generator/spark/AppWriter.scala
@@ -129,7 +129,7 @@ object AppWriter {
         val stringAdd = bufferedSource.getLines().mkString("\n")
         val externaludf = stringAdd.concat(
           s"""\n${u.name}("${u.in.map(codegen.generate(_)).mkString("\",\"")}",
-             |"${u.params.mkString("\",\"")}"),
+             |"${u.params.mkString("\",\"")}")
              |""".stripMargin)
         bufferedSource.close()
         val udfcontents = writeUdfParagraph(externaludf, utype = u.tp.toString())
