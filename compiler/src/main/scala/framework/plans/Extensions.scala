bd924e5e475119e04939310b469074d01a4ae287
PavlosPiperis <58398272+PavlosPiperis@users.noreply.github.com>
2021-08-10 15:20:39 +0300

    random push

--- compiler/src/main/scala/framework/plans/Extensions.scala
+++ compiler/src/main/scala/framework/plans/Extensions.scala
@@ -62,7 +62,7 @@ trait Extensions {
     case u:Nest => getHint(u.in)
     case CNamed(n, e1) => getHint(e1)
     case u:CUdf => Set(u.hint)
-    case _ => sys.error("Missing" + e)
+    case _ => ""
   }
 
   def collectFromAnd(e: CExpr): Set[String] = e match {
