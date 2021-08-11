da68ca0621bc0a09e571910631246b6a4ec40dfc
Jaclyn Smith <jacmarjorie@gmail.com>
2021-08-11 16:05:53 +0100

    working on label inside label issue

--- compiler/src/main/scala/framework/examples/genomic/UdfTest.scala
+++ compiler/src/main/scala/framework/examples/genomic/UdfTest.scala
@@ -6,6 +6,85 @@ import framework.examples.Query
 import framework.nrc.Parser
 // test2
 
+object MultiOmicsProstate extends DriverGene {
+  val sampleFile = "/mnt/app_hdd/data/biospecimen/aliquot/nationwidechildrens.org_biospecimen_aliquot_prad.txt"
+  val cnvFile = "/mnt/app_hdd/data/cnv"
+  val exprFile = "/mnt/app_hdd/data/expression/"
+  val aexprFile = "/mnt/app_hdd/data/fpkm_uq_case_aliquot.txt"
+  val occurFile = "/mnt/app_hdd/data/somatic/"
+  val occurName = "datasetPRAD"
+  val occurDicts = ("odictPrad1", "odictPrad2", "odictPrad3")
+  val pathFile = "/mnt/app_hdd/data/pathway/c2.cp.v7.1.symbols.gmt"
+  val gtfFile = "/mnt/app_hdd/data/genes/Homo_sapiens.GRCh37.87.chr.gtf"
+  val pradFile = "/mnt/app_hdd/data/biospecimen/clinical/nationwidechildrens.org_clinical_patient_prad.txt"
+  val clinDir = "/mnt/app_hdd/data/biospecimen/clinical"
+
+  override def loadTables(shred: Boolean = false, skew: Boolean = false): String =
+    s"""|${loadBiospec(shred, skew, fname = pradFile, name = "clinical", func = "Prad")}
+        |${loadBiospec(shred, skew, fname = sampleFile, name = "samples")}
+        |${loadGeneExpr(shred, skew, fname = exprFile, aname = aexprFile)}
+        |${loadOccurrence(shred, skew, fname = occurFile, iname = occurName, dictNames = occurDicts)}
+        |${loadGtfTable(shred, skew, fname = gtfFile)}
+        |""".stripMargin
+
+  // name to identify your query
+  val name = "ExampleQueryMultiOmicsProstate"
+
+  // a map of input types for the parser
+    val tbls = Map("occurrences" -> occurmids.tp,
+                    "copynumber" -> copynum.tp,
+                    "expression" -> fexpression.tp,
+                    "samples" -> samples.tp,
+                    "pathways" -> pathway.tp,
+                    //"clinical" -> BagType(tcgaType),// this is for the tcga only
+                    "genemap" -> gtf.tp,
+                    "clinical" -> BagType(pradType))
+
+  val query =
+    s"""
+        mapExpression <=
+          for s in samples union
+            for e in expression union
+              if (s.bcr_aliquot_uuid = e.ge_aliquot) then
+                {(sid := s.bcr_patient_uuid, gene := e.ge_gene_id, fpkm := e.ge_fpkm)};
+
+         impactGMB <=
+          for g in genemap union
+            {(gene_name := g.g_gene_name, gene_id:= g.g_gene_id, burdens :=
+              (for o in occurrences union
+                for s in clinical union
+                  if (o.donorId = s.bcr_patient_uuid) then
+                    for t in o.transcript_consequences union
+                      if (g.g_gene_id = t.gene_id) then
+                         {(sid := o.donorId,
+                           lbl := if (s.gleason_pattern_primary = 2) then 0
+                            else if (s.gleason_pattern_primary = 3) then 0
+                            else if (s.gleason_pattern_primary = 4) then 1
+                            else if (s.gleason_pattern_primary = 5) then 1
+                            else -1,
+                           burden := if (t.impact = "HIGH") then 0.80
+                                                    else if (t.impact = "MODERATE") then 0.50
+                                                    else if (t.impact = "LOW") then 0.30
+                                                    else 0.01
+                          )}
+              ).sumBy({sid, lbl}, {burden})
+            )};
+
+        GMB <=
+          for g in impactGMB union
+            {(gene_name := g.gene_name, gene_id := g.gene_id, burdens :=
+              (for b in g.burdens union
+                for e in mapExpression union
+                  if (b.sid = e.sid && g.gene_id = e.gene) then
+                    {(sid := b.sid, lbl := b.lbl, burden := b.burden*e.fpkm)}).sumBy({sid,lbl}, {burden})
+                     )}
+    """
+
+    val parser = Parser(tbls)
+    val program = parser.parse(query).get.asInstanceOf[Program]
+
+}
+
 // this is just the example from the 
 object ExampleQuery extends DriverGene {
   
